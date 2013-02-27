a4f4b8e211b4e96145a84aa02e7997ac52f931fe
Chin Liang See <clsee@altera.com>
2013-02-27 15:21:28 +0800

    Pushed Dinh changes from branch v2012.10
    From Dinh's commit
    
        FogBugz #98277: Enable CMD_BOOTZ in uboot
    
        Enable the capability in uboot to load a standard Linux zImage, instead
        of having going through the extra step of making a uImage.
    
        Starting with Linux kernel v3.7, socfpga has enabled AUTO_ZRELADDR
        that allows Linux to automatically determine the entry address of the
        kernel image. However, this feature only works with the zImage
        not uImage.
    
        Signed-off-by: Dinh Nguyen <dinguyen@altera.com>

--- include/configs/socfpga_cyclone5.h
+++ include/configs/socfpga_cyclone5.h
@@ -65,7 +65,7 @@
 #define CONFIG_SYS_TEXT_BASE		0x01000040
 #endif
 /* Default load address */
-#define CONFIG_SYS_LOAD_ADDR		0x7fc0
+#define CONFIG_SYS_LOAD_ADDR		0x8000
 #ifdef CONFIG_USE_IRQ
 /* Enable board late init for ECC setup if IRQ enabled */
 #define CONFIG_BOARD_LATE_INIT
@@ -144,6 +144,9 @@
 /* OSE operating system support */
 #define CONFIG_BOOTM_OSE
 
+/* Support loading of zImage */
+#define CONFIG_CMD_BOOTZ
+
 
 /*
  * Environment setup
@@ -169,7 +172,7 @@
 #endif
 
 /*
- * arguments passed to the bootm command. The value of
+ * arguments passed to the bootz command. The value of
  * CONFIG_BOOTARGS goes into the environment value "bootargs".
  * Do note the value will overide also the chosen node in FDT blob.
  */
@@ -179,7 +182,7 @@
 	"verify=n\0" \
 	"loadaddr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"fdtaddr=0x00000100\0" \
-	"bootimage=uImage\0" \
+	"bootimage=zImage\0" \
 	"bootimagesize=0x600000\0" \
 	"fdtimage=socfpga.dtb\0" \
 	"fdtimagesize=0x2000\0" \
@@ -192,13 +195,13 @@
 	"qspiroot=/dev/mtdblock1\0" \
 	"qspirootfstype=jffs2\0" \
 	"ramboot=setenv bootargs " CONFIG_BOOTARGS ";" \
-		"bootm ${loadaddr} - ${fdtaddr}\0" \
+		"bootz ${loadaddr} - ${fdtaddr}\0" \
 	"mmcload=mmc rescan;" \
 		"${mmcloadcmd} mmc 0:${mmcloadpart} ${loadaddr} ${bootimage};" \
 		"${mmcloadcmd} mmc 0:${mmcloadpart} ${fdtaddr} ${fdtimage}\0" \
 	"mmcboot=setenv bootargs " CONFIG_BOOTARGS \
 		" root=${mmcroot} rw rootwait;" \
-		"bootm ${loadaddr} - ${fdtaddr}\0" \
+		"bootz ${loadaddr} - ${fdtaddr}\0" \
 	"netboot=dhcp ${bootimage} ; " \
 		"tftp ${fdtaddr} ${fdtimage} ; run ramboot\0" \
 	"qspiload=sf probe ${qspiloadcs};" \
@@ -206,7 +209,7 @@
 		"sf read ${fdtaddr} ${qspifdtaddr} ${fdtimagesize};\0" \
 	"qspiboot=setenv bootargs " CONFIG_BOOTARGS \
 		" root=${qspiroot} rw rootfstype=${qspirootfstype};"\
-		"bootm ${loadaddr} - ${fdtaddr}\0" \
+		"bootz ${loadaddr} - ${fdtaddr}\0" \
 	"fpga=0\0" \
 	"fpgadata=0x2000000\0" \
 	"fpgadatasize=0x700000\0"
