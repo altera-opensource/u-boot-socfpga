191737ccb1214dfd02a3b279549ad4bc0e5218c1
Ching Liang See(PG-IP) <clsee@sj-ice-nx1.altera.com>
2013-06-07 11:31:31 -0700

    FogBugz #129034: Disable SDRAM ECC interrupt to avoid false DBE error
    Reverting / disable SDRAM ECC interrupt support within U-Boot. This is to
    avoid false double bit error which might happen if user using U-Boot
    console to read back uninitialized memory. But this won't impact single
    bit error as it will be corrected by hardware.
    
    Signed-off-by: Chin Liang See <clsee@altera.com>

--- board/altera/socfpga_cyclone5/socfpga_cyclone5.c
+++ board/altera/socfpga_cyclone5/socfpga_cyclone5.c
@@ -304,20 +304,6 @@ int cpu_mmc_init(bd_t *bis)
 #ifdef CONFIG_BOARD_LATE_INIT
 int board_late_init(void)
 {
-#ifndef CONFIG_SOCFPGA_VIRTUAL_TARGET
-#if (CONFIG_HPS_SDR_CTRLCFG_CTRLCFG_ECCEN == 1)
-	/* clear first any pending interrupt */
-	sdram_enable_interrupt(0);
-	/* create event for tracking SDRAM ECC SBE count */
-	setenv_ulong("sdram_ecc_sbe", 0);
-	/* register SDRAM ECC handler */
-	irq_register(IRQ_ECC_SDRAM,
-		irq_handler_ecc_sdram,
-		(void *)&irq_cnt_ecc_sdram, 0);
-	sdram_enable_interrupt(1);
-	puts("SDRAM: ECC Enabled\n");
-#endif 	/* CONFIG_HPS_SDR_CTRLCFG_CTRLCFG_ECCEN */
-#endif
 	return 0;
 }
 #endif
