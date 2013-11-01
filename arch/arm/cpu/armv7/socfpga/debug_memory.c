17e187ae8ca530f5a5ef13855b31a91d4f2a0715
Chin Liang See <clsee@altera.com>
2013-11-01 11:31:54 -0700

    FogBugz #163711: Enhancing Preloader semihosting function
    Enhancing the semihosting function to ensure its working well
    with various compilers. The enhancement will avoid incorrect
    assumption being made between the call stack and function call.
    
    Signed-off-by: Chin Liang See <clsee@altera.com>

--- arch/arm/cpu/armv7/socfpga/debug_memory.c
+++ arch/arm/cpu/armv7/socfpga/debug_memory.c
@@ -137,10 +137,10 @@ defined(CONFIG_SPL_SERIAL_SUPPORT)
 int svc(unsigned operation, void *value)
 {
 	int temp;
-	asm volatile("push {lr}");
-	asm volatile("svc 0x123456");
-	asm volatile("mov %0, r0" : "=r" (temp) : : "cc");
-	asm volatile("pop {lr}");
+	asm volatile ("mov r0, %1; mov r1, %2; svc 0x123456; mov %0, r0"
+		: "=r" (temp)
+		: "r" (operation), "r" (value)
+		: "r0", "r1", "r2", "r3", "ip", "lr", "memory", "cc");
 	return temp;
 }
 
