altera-opensource/u-boot-socfpga

commit f215f9ddc8d989ff85e93a5a8c1b8a843f2773c8

FogBugz #270904-85: allow early use of shared io pins
Fix double call to ddr_calibration_sequence()

Add support for external-fpga-config boolean in the chosen node
of the device tree.  When present, the code will wait forever for the
fpga to be programmed before exiting the function, s_init().  The intention
is to allow early usage of a uart that might be connected to shared ios.

With the sd-card code configuration, and no external-fpga-config property nor
cff_filename property in the chose node,  the code will ignore all fpga
programming and drop to the uboot prompt.  This is intended for use with a uart
connected to dedicated pins or semihosting.  This allows for manually executing
the "fpga loadfs" and "ddrcal" commands.

With the qspi code configuration, external-fpga-config in the chosen
node is honored, and s_init() will not be exited until the fpga
is programed by an external source.  If external-fpga-config is not present
in the chosen node, streaming the fpga image from qspi is attempted.

Signed-off-by: Matthew Gerlach <mgerlach@opensource.altera.com>

arch/arm/cpu/armv7/socfpga_arria10/cff.c
@@ -8,6 +8,8 @@
 #include <common.h>
 #include <asm/io.h>
 #include <asm/arch/fpga_manager.h>
+#include <asm/arch/reset_manager.h>
+#include <asm/arch/misc.h>
 #include <fat.h>
 #include <fs.h>
 #include <mmc.h>
@@ -19,7 +21,7 @@
 DECLARE_GLOBAL_DATA_PTR;
 
 #ifdef CONFIG_MMC
-static const char *get_cff_filename(const void *fdt, int *len)
+const char *get_cff_filename(const void *fdt, int *len)
 {
 	const char *cff_filename = NULL;
 	const char *cell;
@@ -336,10 +338,14 @@ int socfpga_loadfs(Altera_desc *desc, const void *buf, size_t bsize,
 
 		ret = cff_from_mmc_fat(fsinfo->dev_part, fsinfo->filename,
 				       slen);
-		if (ret > 0)
+		if (ret > 0) {
+			config_shared_fpga_pins(gd->fdt_blob);
+			reset_deassert_shared_connected_peripherals();
+			reset_deassert_fpga_connected_peripherals();
 			return FPGA_SUCCESS;
-		else
+		} else {
 			return FPGA_FAIL;
+		}
 	}
 #endif
 #ifdef CONFIG_CADENCE_QSPI
@@ -348,6 +354,9 @@ int socfpga_loadfs(Altera_desc *desc, const void *buf, size_t bsize,
 		u32 rbfaddr = simple_strtoul(fsinfo->dev_part, NULL, 16);
 		ret = cff_from_qspi(rbfaddr);
 		if (ret > 0)
+			config_shared_fpga_pins(gd->fdt_blob);
+			reset_deassert_shared_connected_peripherals();
+			reset_deassert_fpga_connected_peripherals();
 			return FPGA_SUCCESS;
 		else
 			return FPGA_FAIL;

arch/arm/cpu/armv7/socfpga_arria10/misc.c
@@ -14,6 +14,7 @@
 #include <asm/arch/dwmmc.h>
 #include <altera.h>
 #include <dwmmc.h>
+#include <fdtdec.h>
 #include <fpga.h>
 #include <mmc.h>
 #include <netdev.h>
@@ -166,3 +167,16 @@ void skip_relocation(void)
 	board_init_r(id, (CONFIG_SYS_INIT_SP_ADDR - CONFIG_OCRAM_STACK_SIZE));
 }
 
+int is_external_fpga_config(const void *blob)
+{
+	int node, len;
+	int rval = 0;
+
+	node = fdt_subnode_offset(blob, 0, "chosen");
+	if (node >= 0) {
+		if (fdt_getprop(blob, node, "external-fpga-config", &len))
+			rval = 1;
+	}
+
+	return rval;
+}

arch/arm/cpu/armv7/socfpga_arria10/s_init.c
@@ -7,10 +7,12 @@
 #include <common.h>
 #include <asm/io.h>
 #include <watchdog.h>
+#include <asm/arch/fpga_manager.h>
 #include <asm/arch/reset_manager.h>
 #include <asm/arch/system_manager.h>
 #include <asm/arch/clock_manager.h>
 #include <asm/arch/ecc_ram.h>
+#include <asm/arch/misc.h>
 #include <asm/arch/sdram.h>
 #include <asm/sections.h>
 #include <fdtdec.h>
@@ -184,7 +186,12 @@ void s_init(void)
 	/* configure the Reset Manager */
 	reset_deassert_dedicated_peripherals();
 
-	/* If fpga is already loaded, calibrate ddr and enable
-	   fpga bridges */
-	ddr_calibration_sequence();
+	if (is_external_fpga_config(gd->fdt_blob)) {
+		while (!is_fpgamgr_user_mode())
+			;
+
+		config_shared_fpga_pins(gd->fdt_blob);
+		reset_deassert_shared_connected_peripherals();
+		reset_deassert_fpga_connected_peripherals();
+	}
 }

arch/arm/cpu/armv7/socfpga_arria10/sdram.c
@@ -18,7 +18,6 @@
 #include <asm/arch/sdram.h>
 #include <asm/arch/system_manager.h>
 
-int config_shared_fpga_pins(const void *blob);
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -1096,18 +1095,6 @@ int ddr_calibration_sequence(void)
 
 	WATCHDOG_RESET();
 
-	reset_assert_uart();
-
-	config_shared_fpga_pins(gd->fdt_blob);
-
-	reset_deassert_uart();
-	reset_deassert_shared_connected_peripherals();
-	reset_deassert_fpga_connected_peripherals();
-
-	NS16550_init((NS16550_t)CONFIG_SYS_NS16550_COM1,
-		     ns16550_calc_divisor((NS16550_t)CONFIG_SYS_NS16550_COM1,
-					   CONFIG_SYS_NS16550_CLK,
-					   CONFIG_BAUDRATE));
 
 	of_get_sdr_cfg(gd->fdt_blob, &cfg);
 
@@ -1145,6 +1132,7 @@ int dram_init(void)
 {
 	bd_t *bd;
 	unsigned long addr;
+	int rval = 0;
 
 	WATCHDOG_RESET();
 
@@ -1179,18 +1167,38 @@ int dram_init(void)
 		- CONFIG_OCRAM_STACK_SIZE - CONFIG_OCRAM_MALLOC_SIZE;
 	mem_malloc_init(malloc_start, CONFIG_OCRAM_MALLOC_SIZE);
 
-#ifdef CONFIG_MMC
-	mmc_initialize(gd->bd);
+	if (is_external_fpga_config(gd->fdt_blob)) {
+		ddr_calibration_sequence();
+	} else {
+#if defined(CONFIG_MMC)
+		int len = 0;
+		const char *cff = get_cff_filename(gd->fdt_blob, &len);
+		if (cff && (len > 0)) {
+			mmc_initialize(gd->bd);
 
-	cff_from_mmc_fat_dt();
+			rval = cff_from_mmc_fat("0:1", cff, len);
+		}
+#elif defined(CONFIG_CADENCE_QSPI)
+		rval = cff_from_qspi_env();
+#else
+#error "unsupported config"
 #endif
-#ifdef CONFIG_CADENCE_QSPI
-	/* do I need this if, or just superflous? */
-	if (!is_fpgamgr_user_mode())
-		cff_from_qspi_env();
-#endif
+		if (rval > 0) {
+			reset_assert_uart();
+			config_shared_fpga_pins(gd->fdt_blob);
+			reset_deassert_uart();
 
-	ddr_calibration_sequence();
+			reset_deassert_shared_connected_peripherals();
+			reset_deassert_fpga_connected_peripherals();
+			NS16550_init((NS16550_t)CONFIG_SYS_NS16550_COM1,
+				     ns16550_calc_divisor(
+					     (NS16550_t)CONFIG_SYS_NS16550_COM1,
+					     CONFIG_SYS_NS16550_CLK,
+					     CONFIG_BAUDRATE));
+
+			ddr_calibration_sequence();
+		}
+	}
 
 	/* Skip relocation as U-Boot cannot run on SDRAM for secure boot */
 	skip_relocation();
@@ -1222,4 +1230,3 @@ void dram_bank_mmu_setup(int bank)
 	set_section_dcache(i, DCACHE_WRITEBACK);
 #endif
 }
-

arch/arm/dts/socfpga_arria10.dts
@@ -13,6 +13,7 @@
 	chosen {
 		cff-file = "ghrd_10as066n2.periph.rbf",
 			"ghrd_10as066n2.core.rbf";
+		/* external-fpga-config; */
 	};
 
 	clocks {

arch/arm/dts/socfpga_arria10_dedicated_uart.dts
@@ -13,6 +13,7 @@
 	chosen {
 		cff-file = "ghrd_10as066n2.periph.rbf",
 			"ghrd_10as066n2.core.rbf";
+		/* external-fpga-config; */
 	};
 
 	clocks {

arch/arm/dts/socfpga_arria10_qspi.dts
@@ -11,7 +11,7 @@
 
 
 	chosen {
-		cff-file = "soc_system.rbf";
+		/* external-fpga-config; */
 	};
 
 	clocks {

arch/arm/dts/socfpga_arria10_qspi_dedicated_uart.dts
@@ -11,6 +11,7 @@
 
 
 	chosen {
+		/* external-fpga-config; */
 	};
 
 	clocks {

commit 211a7fbf5b4ea05db7877ea898af70480abca424

FogBugz #270904-89: commit: f215f9ddc8d989ff85e93a5a8c1b8a843f2773c8 broke compile for qspi defconfig. This patch fixes the problem.
Signed-off-by: Graham Moore <grmoore@opensource.altera.com>

arch/arm/cpu/armv7/socfpga_arria10/cff.c
@@ -353,13 +353,14 @@ int socfpga_loadfs(Altera_desc *desc, const void *buf, size_t bsize,
 		int ret;
 		u32 rbfaddr = simple_strtoul(fsinfo->dev_part, NULL, 16);
 		ret = cff_from_qspi(rbfaddr);
-		if (ret > 0)
+		if (ret > 0) {
 			config_shared_fpga_pins(gd->fdt_blob);
 			reset_deassert_shared_connected_peripherals();
 			reset_deassert_fpga_connected_peripherals();
 			return FPGA_SUCCESS;
-		else
+		} else {
 			return FPGA_FAIL;
+		}
 	}
 #endif
 