a2797d73a1eac332f9ed9a3e440b9cbae82e0156
Chin Liang See <chin.liang.see@intel.com>
2017-11-20 09:10:06 +0800

    FogBugz #511271: arm: socfpga: stratix10: Remove hard code of clock source frequency
    To fix the clock driver to grab the clock source frequency from
    handoff binary blob.
    
    Signed-off-by: Chin Liang See <chin.liang.see@intel.com>

--- arch/arm/mach-socfpga/wrap_pll_config_s10.c
+++ arch/arm/mach-socfpga/wrap_pll_config_s10.c
@@ -8,6 +8,10 @@
 #include <asm/arch/clock_manager.h>
 #include <asm/io.h>
 #include <asm/arch/handoff_s10.h>
+#include <asm/arch/system_manager.h>
+
+static const struct socfpga_system_manager *sysmgr_regs =
+	(struct socfpga_system_manager *)SOCFPGA_SYSMGR_ADDRESS;
 
 const struct cm_config * const cm_get_default_config(void)
 {
@@ -32,7 +36,11 @@ const struct cm_config * const cm_get_default_config(void)
 
 const unsigned int cm_get_osc_clk_hz(const int osc)
 {
-	return 25000000;
+#ifdef CONFIG_SPL_BUILD
+	u32 clock = readl(CONFIG_HANDOFF_CLOCK_OSC);
+	writel(clock, &sysmgr_regs->boot_scratch_cold1);
+#endif
+	return readl(&sysmgr_regs->boot_scratch_cold1);
 }
 
 const unsigned int cm_get_intosc_clk_hz(void)
@@ -43,6 +51,9 @@ const unsigned int cm_get_intosc_clk_hz(void)
 
 const unsigned int cm_get_fpga_clk_hz(void)
 {
-	/* assuming 50MHz */
-	return 50000000;
+#ifdef CONFIG_SPL_BUILD
+	u32 clock = readl(CONFIG_HANDOFF_CLOCK_FPGA);
+	writel(clock, &sysmgr_regs->boot_scratch_cold2);
+#endif
+	return readl(&sysmgr_regs->boot_scratch_cold2);
 }
