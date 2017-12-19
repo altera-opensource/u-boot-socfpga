ab221d6b0903645892fb19e3889cd990024ffabe
Thor Thayer <thor.thayer@linux.intel.com>
2017-12-19 10:14:59 -0600

    FogBugz #514871: arm: socfpga: stratix10: Enable SPI1 support
    Enable SPIM1 support for SOCFPGA Stratix10 SoC Daughtercard or
    SPI Loopback testing.
    
    Signed-off-by: Thor Thayer <thor.thayer@linux.intel.com>

--- arch/arm/mach-socfpga/spl_s10.c
+++ arch/arm/mach-socfpga/spl_s10.c
@@ -139,6 +139,8 @@ void board_init_f(ulong dummy)
 	socfpga_per_reset(SOCFPGA_RESET(USB1), 0);
 	socfpga_per_reset(SOCFPGA_RESET(USB0_OCP), 0);
 	socfpga_per_reset(SOCFPGA_RESET(USB1_OCP), 0);
+	/* enable SPIM1 */
+	socfpga_per_reset(SOCFPGA_RESET(SPIM1), 0);
 
 	/* disable lwsocf2fpga and soc2fpga bridge security */
 	writel(FIREWALL_BRIDGE_DISABLE_ALL, SOCFPGA_FIREWALL_SOC2FPGA);
