eb4e90cd83cf0a07b957cc0ac358d16c6b3bd45b
Thor Thayer <tthayer@altera.com>
2018-04-04 13:42:46 -0700

    FogBugz #548210-3: Refactor SMC shared code into common header
    The common macros are pulled into a separate header file so that
    additional SMC functions can reference them.
    
    Signed-off-by: Thor Thayer <thor.thayer@linux.intel.com>
    ---
    v2  No change except for different patch number (-3)

--- arch/arm/mach-socfpga/smc_fpga_reconfig_s10.c
+++ arch/arm/mach-socfpga/smc_fpga_reconfig_s10.c
@@ -10,45 +10,11 @@
 #include <asm/psci.h>
 #include <asm/secure.h>
 #include <asm/arch/mailbox_s10.h>
+#include <asm/arch/smc_s10.h>
 #include <linux/intel-smc.h>
 
 #define FPGA_CONFIG_RESEVED_MEM_START		0x1000
 #define FPGA_CONFIG_RESERVED_MEM_SIZE		0x1000000	/* 16 MB */
-
-#define SMC_ARG0		0
-#define SMC_ARG1		(SMC_ARG0 + 1)
-#define SMC_ARG2		(SMC_ARG1 + 1)
-#define SMC_ARG3		(SMC_ARG2 + 1)
-#define SMC_RETURN_ARGS_MAX	(SMC_ARG3 + 1)
-
-/* Macro functions for allocation and read/write of
-   variables to be assigned to registers */
-/* Allocate memory for variable */
-#define SMC_ALLOC_REG_MEM(var) unsigned long var[SMC_RETURN_ARGS_MAX]
-/* Clear variable */
-#define SMC_INIT_REG_MEM(var) \
-	do { \
-		int i; \
-		for (i = 0; i < SMC_RETURN_ARGS_MAX; i++) \
-			var[i] = 0; \
-	} while (0)
-/* Read variable */
-#define SMC_GET_REG_MEM(var, i) var[i]
-/* Write Variable */
-#define SMC_ASSIGN_REG_MEM(var, i, val) \
-	do { \
-		var[i] = (val); \
-	} while (0)
-/* Assign variables back to registers */
-#define SMC_RET_REG_MEM(var) \
-	do { \
-		asm volatile("ldr x0, %0\n" \
-			     "ldr x1, %1\n" \
-			     "ldr x2, %2\n" \
-			     "ldr x3, %3\n" \
-			     : : "m" (var[0]), "m" (var[1]), \
-				 "m" (var[2]), "m" (var[3]) : ); \
-	} while (0)
 
 #define FPGA_CONFIG_BUF_MAX			16
 
