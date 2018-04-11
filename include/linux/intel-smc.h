70bb2e02ade516ff503db1a732b65a7505edcf67
Thor Thayer <tthayer@altera.com>
2018-04-11 06:31:47 -0700

    FogBugz #548210-2: Add SysMan and ECC Reads to intel-smc.h
    Add the System Manager and ECC reads and writes to the intel-smc.h
    file.
    
    Signed-off-by: Thor Thayer <thor.thayer@linux.intel.com>
    ---
    v2  Separate patch for this addition.

--- include/linux/intel-smc.h
+++ include/linux/intel-smc.h
@@ -66,12 +66,21 @@
  *
  * INTEL_SIP_SMC_FPGA_CONFIG_STATUS_ERROR:
  * There is error during the FPGA configuration process.
+ *
+ * INTEL_SIP_SMC_SYSMAN_ERROR:
+ * There is error during a read or write operation of the System Manager
+ * registers.
+ *
+ * INTEL_SIP_SMC_ECC_ERROR:
+ * There is error during a read or write operation of the ECC registers.
  */
 #define INTEL_SIP_SMC_RETURN_UNKNOWN_FUNCTION		0xFFFFFFFF
 #define INTEL_SIP_SMC_STATUS_OK				0x0
 #define INTEL_SIP_SMC_FPGA_CONFIG_STATUS_BUSY		0x1
 #define INTEL_SIP_SMC_FPGA_CONFIG_STATUS_REJECTED       0x2
 #define INTEL_SIP_SMC_FPGA_CONFIG_STATUS_ERROR		0x4
+#define INTEL_SIP_SMC_SYSMAN_ERROR			0x5
+#define INTEL_SIP_SMC_ECC_ERROR				0x6
 
 /*
  * Request INTEL_SIP_SMC_FPGA_CONFIG_START
@@ -201,5 +210,87 @@ INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FPGA_CONFIG_COMPLETED_WRITE)
 #define INTEL_SIP_SMC_FUNCID_FPGA_CONFIG_LOOPBACK 6
 #define INTEL_SIP_SMC_FPGA_CONFIG_LOOPBACK \
 	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_FPGA_CONFIG_LOOPBACK)
+
+/*
+ * Request INTEL_SIP_SMC_SYSMAN_READ
+ *
+ * Read a register in the system manager at a fixed offset.
+ *
+ * Call register usage:
+ * a0: INTEL_SIP_SMC_FUNCID_SYSMAN_READ.
+ * a1: register offset from beginning of System Manager block.
+ * a2-7: not used.
+ *
+ * Return status:
+ * a0: INTEL_SIP_SMC_STATUS_OK or INTEL_SIP_SMC_SYSMAN_ERROR.
+ * a1: Value in the register
+ * a2-3: not used.
+ */
+#define INTEL_SIP_SMC_FUNCID_SYSMAN_READ 7
+#define INTEL_SIP_SMC_SYSMAN_READ \
+	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_SYSMAN_READ)
+
+/*
+ * Request INTEL_SIP_SMC_SYSMAN_WRITE
+ *
+ * Write a register in the system manager at a fixed offset.
+ *
+ * Call register usage:
+ * a0: INTEL_SIP_SMC_FUNCID_SYSMAN_WRITE.
+ * a1: register offset from beginning of System Manager block.
+ * a2: mask to indicate which bits change.
+ * a3: value to program into register.
+ * a4-7: not used.
+ *
+ * Return status:
+ * a0: INTEL_SIP_SMC_STATUS_OK or INTEL_SIP_SMC_SYSMAN_ERROR.
+ * a1: Value in the register
+ * a2-3: not used.
+ */
+#define INTEL_SIP_SMC_FUNCID_SYSMAN_WRITE 8
+#define INTEL_SIP_SMC_SYSMAN_WRITE \
+	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_SYSMAN_WRITE)
+
+/*
+ * Request INTEL_SIP_SMC_ECC_HMC_READ
+ *
+ * For ECC operation, we need to read the Hard Memory controller ECC
+ * registers.
+ *
+ * Call register usage:
+ * a0: INTEL_SIP_SMC_FUNCID_ECC_HMC_READ.
+ * a1: Byte offset from ECC_CTRL1 (up to 0xC0)
+ * a2-7: not used.
+ *
+ * Return status:
+ * a0: INTEL_SIP_SMC_STATUS_OK or INTEL_SIP_SMC_ECC_ERROR.
+ * a1: Value in the HMC ECC Offset register
+ * a2-3: Not used.
+ */
+#define INTEL_SIP_SMC_FUNCID_ECC_HMC_READ 9
+#define INTEL_SIP_SMC_ECC_HMC_READ \
+	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_ECC_HMC_READ)
+
+/*
+ * Request INTEL_SIP_SMC_ECC_HMC_WRITE
+ *
+ * For ECC operation, we need to write the Hard Memory controller ECC
+ * registers.
+ *
+ * Call register usage:
+ * a0: INTEL_SIP_SMC_FUNCID_ECC_HMC_WRITE.
+ * a1: Byte offset from ECC_CTRL1 (up to 0xC0)
+ * a2: Write Mask.
+ * a3: Value to write.
+ * a4-7: not used.
+ *
+ * Return status:
+ * a0: INTEL_SIP_SMC_STATUS_OK or INTEL_SIP_SMC_ECC_ERROR.
+ * a1: Value in the HMC ECC Offset register
+ * a2-3: Not used.
+ */
+#define INTEL_SIP_SMC_FUNCID_ECC_HMC_WRITE 10
+#define INTEL_SIP_SMC_ECC_HMC_WRITE \
+	INTEL_SIP_SMC_FAST_CALL_VAL(INTEL_SIP_SMC_FUNCID_ECC_HMC_WRITE)
 
 #endif
