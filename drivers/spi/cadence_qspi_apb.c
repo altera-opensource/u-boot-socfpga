3c981ba6ee1cb3f6d156b0afeefa1cb29ce2b134
Chee Hong Ang <chee.hong.ang@intel.com>
2018-09-06 16:20:05 +0800

    FogBugz #516535: Fix QSPI write issues
    QSPI driver perform chip select on every flash read/write
    access. The driver need to disable/enable the QSPI controller
    while performing chip select. This may cause some data lost
    especially the QSPI controller is configured to run at slower
    speed as it may take longer time to access the flash device.
    This patch prevent the driver from disable/enable the QSPI
    controller too soon and inadvertently halting any ongoing flash
    read/write access by ensuring the QSPI controller is always in
    idle mode after each read/write access.
    
    Signed-off-by: Chee Hong Ang <chee.hong.ang@intel.com>

--- drivers/spi/cadence_qspi_apb.c
+++ drivers/spi/cadence_qspi_apb.c
@@ -648,6 +648,10 @@ cadence_qspi_apb_indirect_read_execute(struct cadence_spi_platdata *plat,
 	writel(CQSPI_REG_INDIRECTRD_DONE,
 	       plat->regbase + CQSPI_REG_INDIRECTRD);
 
+	/* Wait til QSPI is idle */
+	if (!cadence_qspi_wait_idle(plat->regbase))
+		return -EIO;
+
 	return 0;
 
 failrd:
@@ -763,6 +767,11 @@ cadence_qspi_apb_indirect_write_execute(struct cadence_spi_platdata *plat,
 	       plat->regbase + CQSPI_REG_INDIRECTWR);
 	if (bounce_buf)
 		free(bounce_buf);
+
+	/* Wait til QSPI is idle */
+	if (!cadence_qspi_wait_idle(plat->regbase))
+		return -EIO;
+
 	return 0;
 
 failwr:
