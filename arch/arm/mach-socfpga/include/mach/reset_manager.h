5e2d6d4bc319d1748f9f7266aedb6b34faebd68d
Chee Hong Ang <chee.hong.ang@intel.com>
2018-09-06 18:18:55 +0800

    FogBugz #583059-1: Enable L2 reset on S10
    Put all slave CPUs (CPU1-3) into WFI mode. Master CPU (CPU0) writes
    the magic word into system manager's scratch register to indicate
    the system has performed L2 reset and request reset manager to
    perform hardware handshake and then trigger L2 reset. CPU0 put
    itself into WFI mode. L2 reset will reboot all HPS CPU cores after
    which all HPS cores are in WFI mode. L2 reset is followed by warm
    reset request by SPL via RMR_EL3 system register.
    To trigger L2 + warm reset under u-boot, set 'reset=warm' in the
    u-boot environment then input 'reset' command in the u-boot command
    prompt.
    
    Signed-off-by: Chee Hong Ang <chee.hong.ang@intel.com>

--- arch/arm/mach-socfpga/include/mach/reset_manager.h
+++ arch/arm/mach-socfpga/include/mach/reset_manager.h
@@ -7,6 +7,7 @@
 #define _RESET_MANAGER_H_
 
 void reset_cpu(ulong addr);
+void l2_reset_cpu(void);
 
 void socfpga_per_reset(u32 reset, int set);
 void socfpga_per_reset_all(void);
--- arch/arm/mach-socfpga/reset_manager.c
+++ arch/arm/mach-socfpga/reset_manager.c
@@ -24,11 +24,26 @@ static const struct socfpga_reset_manager *reset_manager_base =
  */
 void reset_cpu(ulong addr)
 {
-	/* request a warm reset */
 #if defined(CONFIG_TARGET_SOCFPGA_STRATIX10)
-	puts("Mailbox: Issuing mailbox cmd REBOOT_HPS\n");
-	mbox_reset_cold();
+#ifndef CONFIG_SPL_BUILD
+	const char *reset = env_get("reset");
+
+	if (reset && !strcmp(reset, "warm")) {
+		/* flush dcache */
+		flush_dcache_all();
+
+		/* request a warm reset */
+		puts("Do warm reset now...\n");
+		l2_reset_cpu();
+	} else {
+#endif
+		puts("Mailbox: Issuing mailbox cmd REBOOT_HPS\n");
+		mbox_reset_cold();
+#ifndef CONFIG_SPL_BUILD
+	}
+#endif
 #else
+	/* request a warm reset */
 	writel(1 << RSTMGR_CTRL_SWWARMRSTREQ_LSB,
 	       &reset_manager_base->ctrl);
 #endif
@@ -38,4 +53,38 @@ void reset_cpu(ulong addr)
 	 */
 	while (1)
 		;
+}
+
+void l2_reset_cpu(void)
+{
+	asm volatile(
+		"str	%0, [%1]\n"
+		/* Increase timeout in rstmgr.hdsktimeout */
+		"ldr	x2, =0xFFFFFF\n"
+		"str	w2, [%2, #0x64]\n"
+		"str	w2, [%2, #0x10]\n"
+		/*
+		 * Set l2flushen = 1, etrstallen = 1,
+		 * fpgahsen = 1 and sdrselfrefen = 1
+		 * in rstmgr.hdsken to perform handshake
+		 * in certain peripherals before trigger
+		 * L2 reset.
+		 */
+		"ldr	x3, =0x10D\n"
+		"orr	x2, x2, x3\n"
+		"str	w2, [%2, #0x10]\n"
+		/* Trigger L2 reset in rstmgr.coldmodrst */
+		"ldr	w2, [%2, #0x34]\n"
+		"orr	x2, x2, #0x100\n"
+		"isb\n"
+		"dsb	sy\n"
+		"str	w2, [%2, #0x34]\n"
+		/* Put all cores into WFI mode */
+		"wfi_loop:\n"
+		"	wfi\n"
+		"	b	wfi_loop\n"
+		: : "r" (L2_RESET_DONE_STATUS),
+		    "r" (L2_RESET_DONE_REG),
+		    "r" (SOCFPGA_RSTMGR_ADDRESS)
+		: "x1", "x2", "x3");
 }
