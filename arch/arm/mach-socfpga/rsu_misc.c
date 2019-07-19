a7427aec74a4a2cbda047d81b50d7a8859dcf9a2
radu bacrau <radu.bacrau@intel.com>
2019-07-19 15:43:42 +0000

    HSD #1707045086: Add support for updating factory image
    Added API for updating the factory image. Changed the algorithm for
    updating the image pointers. Added command for calling the new API
    from CLI.
    
    Signed-off-by: Radu Bacrau <radu.bacrau@intel.com>

--- arch/arm/mach-socfpga/rsu_misc.c
+++ arch/arm/mach-socfpga/rsu_misc.c
@@ -42,6 +42,79 @@ struct pointer_block {
 };
 
 /**
+ * enum rsu_block_type - enumeration for image block types
+ * @SECTION_BLOCK: section block
+ * @SIGNATURE_BLOCK: signature block
+ * @REGULAR_BLOCK: all other block types
+ */
+enum rsu_block_type {
+	SECTION_BLOCK = 0,
+	SIGNATURE_BLOCK,
+	REGULAR_BLOCK
+};
+
+/* maximum number of sections supported for an image */
+#define MAX_SECTIONS 64
+
+/**
+ * struct rsu_image_state - structure for stated of image processing
+ * @offset: current block offset in bytes
+ * @block_type: current block type
+ * @sections: identified section offsets
+ * @no_sections: number of identified sections
+ * @absolute: current image is an absolute image
+ *
+ * This structure is used to maintain the state of image parsing, both for
+ * relocating images to final destination in flash, and also for verifying
+ * images already stored in flash.
+ */
+struct rsu_image_state {
+	int offset;
+	enum rsu_block_type block_type;
+	u64 sections[MAX_SECTIONS];
+	int no_sections;
+	int absolute;
+};
+
+/**
+ * find_section() - search section in the current list of identified sections
+ * @state: current state machine state
+ * @section: section to be searched
+ *
+ * Return: 1 if section is found, 0 if section is not found
+ */
+static int find_section(struct rsu_image_state *state, u64 section)
+{
+	int x;
+
+	for (x = 0; x < state->no_sections; x++)
+		if (section == state->sections[x])
+			return 1;
+
+	return 0;
+}
+
+/**
+ * add_section() - add section to the current list of identified sections
+ * @state: current state machine state
+ * @section: section to be added
+ *
+ * Return: zero value for success, -1 on error
+ */
+static int add_section(struct rsu_image_state *state, u64 section)
+{
+	if (find_section(state, section))
+		return 0;
+
+	if (state->no_sections >= MAX_SECTIONS)
+		return -1;
+
+	state->sections[state->no_sections++] = section;
+
+	return 0;
+}
+
+/**
  * swap_bits() - swap bits
  * @data: pointer point to data
  * @len: data length
@@ -61,85 +134,6 @@ static void swap_bits(char *data, int len)
 		}
 		data[x] = tmp;
 	}
-}
-
-/**
- * rsu_misc_image_adjust() - adjust values in the pointer block
- *
- * @block - pointer to the start of the second 4KB block of an image
- * @info - rsu_slot_info structure for target slot
- *
- * Adjust values in the 256 byte pointer block for the offset of the
- * slot being programmed. The pointer block is in the second 4KB block
- * of the image. The pointer block contains a CRC of the entire 4KB block.
- *
- * Returns 0 on success and block was adjusted, or -1 on error
- */
-int rsu_misc_image_adjust(void *block, struct rsu_slot_info *info)
-{
-	u32 calc_crc;
-	int x;
-	char *data = (char *)block;
-	struct pointer_block *ptr_blk = (struct pointer_block *)(data
-					 + IMAGE_PTR_START
-					 - IMAGE_PTR_BLOCK);
-
-	/*
-	 * Check CRC on 4KB block before proceeding.  All bytes must be
-	 * bit-swapped before they can used in zlib CRC32 library function
-	 * The CRC value is stored in big endian in the bitstream.
-	 */
-	swap_bits(block, IMAGE_BLOCK_SZ);
-	calc_crc = crc32(0, block, IMAGE_PTR_CRC - IMAGE_BLOCK_SZ);
-	if (be32_to_cpu(ptr_blk->crc) != calc_crc) {
-		rsu_log(RSU_ERR, "Bad CRC. Calc = %08X / Fm Block = %08x",
-			calc_crc, be32_to_cpu(ptr_blk->crc));
-		return -1;
-	}
-	swap_bits(block, IMAGE_BLOCK_SZ);
-
-	/*
-	 * Adjust main image pointers after they pass a validity test.
-	 * Return -1 if an error is found, or 0 if the block looks OK
-	 * (adjusted or not adjusted)
-	 */
-	if (ptr_blk->num_ptrs == 0)
-		return 0;
-
-	if (ptr_blk->num_ptrs > 4) {
-		rsu_log(RSU_ERR, "Invalid number of pointers in block\n");
-		return -1;
-	}
-
-	for (x = 0; x < ptr_blk->num_ptrs; x++) {
-		if (ptr_blk->ptrs[x] > (u64)info->size) {
-			rsu_log(RSU_DEBUG, "A ptr > 0x%llX, not adjust\n",
-				(u64)info->size);
-
-			for (x = 0; x < ptr_blk->num_ptrs; x++) {
-				if (ptr_blk->ptrs[x] < info->offset ||
-				    ptr_blk->ptrs[x] >=
-				    (info->offset + info->size)) {
-					rsu_log(RSU_ERR,
-						"ptr not in the slot\n");
-					return -1;
-				}
-			}
-
-			return 0;
-		}
-	}
-
-	for (x = 0; x < ptr_blk->num_ptrs; x++)
-		ptr_blk->ptrs[x] += info->offset;
-
-	/* Update CRC in block */
-	swap_bits(block, IMAGE_BLOCK_SZ);
-	calc_crc = crc32(0, block, IMAGE_PTR_CRC - IMAGE_BLOCK_SZ);
-	ptr_blk->crc = cpu_to_be32(calc_crc);
-	swap_bits(block, IMAGE_BLOCK_SZ);
-
-	return 0;
 }
 
 /**
@@ -326,6 +320,285 @@ int rsu_cb_buf(void *buf, int len)
 }
 
 /**
+ * sig_block_process() - process signature block
+ * @state: current state machine state
+ * @block: signature block
+ * @info: slot where the data will be written
+ *
+ * Determine if the signature block is part of an absolute image, and add its
+ * section pointers to the list of identified sections.
+ *
+ * Return: zero value for success, or negative value on error
+ */
+static int sig_block_process(struct rsu_image_state *state,	void *block,
+			     struct rsu_slot_info *info)
+{
+	char *data = (char *)block;
+	struct pointer_block *ptr_blk = (struct pointer_block *)(data
+					+ SIG_BLOCK_PTR_OFFS);
+	int x;
+
+	/* Determine if absolute image - only done for 2nd block in an image
+	 * which is always a signature block
+	 */
+	if (state->offset == IMAGE_BLOCK_SZ)
+		for (x = 0; x < 4; x++)
+			if (ptr_blk->ptrs[x] > (__u64)info->size) {
+				state->absolute = 1;
+				rsu_log(RSU_DEBUG, "Found absolute image\n");
+				break;
+			}
+
+	/* Add pointers to list of identified sections */
+	for (x = 0; x < 4; x++)
+		if (ptr_blk->ptrs[x]) {
+			if (state->absolute)
+				add_section(state, ptr_blk->ptrs[x] -
+					    info->offset);
+			else
+				add_section(state, ptr_blk->ptrs[x]);
+		}
+
+	return 0;
+}
+
+/**
+ * sig_block_adjust() - adjust signature block pointers before writing to flash
+ * @state: current state machine state
+ * @block: signature block
+ * @info: slot where the data will be written
+ *
+ * This function checks that the section pointers are consistent, and for non-
+ * absolute images it updates them to match the destination slot, also re-
+ * computing the CRC.
+ *
+ * Return: zero value for success, -1 on error
+ */
+static int sig_block_adjust(struct rsu_image_state *state, void *block,
+			    struct rsu_slot_info *info)
+{
+	u32 calc_crc;
+	int x;
+	char *data = (char *)block;
+	struct pointer_block *ptr_blk = (struct pointer_block *)(data
+					+ SIG_BLOCK_PTR_OFFS);
+
+	/*
+	 * Check CRC on 4kB block before proceeding.  All bytes must be
+	 * bit-swapped before they can used in zlib CRC32 library function.
+	 * The CRC value is stored in big endian in the bitstream.
+	 */
+	swap_bits(block, IMAGE_BLOCK_SZ);
+	calc_crc = crc32(0, (uchar *)block, SIG_BLOCK_CRC_OFFS);
+	if (be32_to_cpu(ptr_blk->crc) != calc_crc) {
+		rsu_log(RSU_ERR,
+			"Error: Bad CRC32. Calc = %08X / From Block = %08x\n",
+			calc_crc, be32_to_cpu(ptr_blk->crc));
+		return -1;
+	}
+	swap_bits(block, IMAGE_BLOCK_SZ);
+
+	/* Check pointers */
+	for (x = 0; x < 4; x++) {
+		u64 ptr = ptr_blk->ptrs[x];
+
+		if (!ptr)
+			continue;
+
+		if (state->absolute)
+			ptr -= info->offset;
+
+		if (ptr > info->size) {
+			rsu_log(RSU_ERR,
+				"Error: A pointer not within the slot\n");
+			return -1;
+		}
+	}
+
+	/* Absolute images do not require pointer updates */
+	if (state->absolute)
+		return 0;
+
+	/* Update pointers */
+	for (x = 0; x < 4; x++) {
+		if (ptr_blk->ptrs[x]) {
+			u64 old =  ptr_blk->ptrs[x];
+
+			ptr_blk->ptrs[x] += info->offset;
+			rsu_log(RSU_DEBUG,
+				"Adjusting pointer 0x%llx -> 0x%llx\n",
+				old, ptr_blk->ptrs[x]);
+		}
+	}
+
+	/* Update CRC in block */
+	swap_bits(block, IMAGE_BLOCK_SZ);
+	calc_crc = crc32(0, (uchar *)block, SIG_BLOCK_CRC_OFFS);
+	ptr_blk->crc = be32_to_cpu(calc_crc);
+	swap_bits(block, IMAGE_BLOCK_SZ);
+
+	return 0;
+}
+
+/**
+ * block_compare() - compare two image blocks
+ * @state: current state machine state
+ * @block: input data provided by user
+ * @vblock: verification data read from flash
+ *
+ * Return: non-negative value for successful comparisor, or negative value on
+ * failure or comparison difference found.
+ */
+static int block_compare(struct rsu_image_state *state, void *block,
+			 void *vblock)
+{
+	char *buf = (char *)block;
+	char *vbuf = (char *)vblock;
+	int x;
+
+	for (x = 0; x < IMAGE_BLOCK_SZ; x++)
+		if (vbuf[x] != buf[x]) {
+			rsu_log(RSU_ERR, "Expect %02X, got %02X @0x%08X\n",
+				buf[x], vbuf[x], state->offset + x);
+			return -ECMP;
+		}
+
+	return 0;
+}
+
+/**
+ * sig_block_compare() - compare two signature blocks
+ * @state: current state machine state
+ * @ublock: input data provided by user
+ * @vblock: verification data read from flash
+ * @info: slot where the verification data was read from
+ *
+ * Absolute images are compared directly, while for non-absolute images the
+ * pointers and associated CRC are re-computed to see if they match.
+ *
+ * Return: zero for success, or negative value on erorr or finding differences.
+ */
+static int sig_block_compare(struct rsu_image_state *state, void *ublock,
+			     void *vblock, struct rsu_slot_info *info)
+{
+	u32 calc_crc;
+	int x;
+	char block[IMAGE_BLOCK_SZ];
+	struct pointer_block *ptr_blk = (struct pointer_block *)(block +
+		SIG_BLOCK_PTR_OFFS);
+
+	rsu_log(RSU_DEBUG, "Compare signature block @0x%08x\n", state->offset);
+
+	/* Make a copy of the data provided by the user */
+	memcpy(block, ublock, IMAGE_BLOCK_SZ);
+
+	/* Update signature block to match what we expect in flash */
+	if (!state->absolute) {
+		/* Update pointers */
+		for (x = 0; x < 4; x++)
+			if (ptr_blk->ptrs[x])
+				ptr_blk->ptrs[x] += info->offset;
+
+		/* Update CRC in block */
+		swap_bits(block, IMAGE_BLOCK_SZ);
+		calc_crc = crc32(0, (uchar *)block, SIG_BLOCK_CRC_OFFS);
+		ptr_blk->crc = be32_to_cpu(calc_crc);
+		swap_bits(block, IMAGE_BLOCK_SZ);
+	}
+
+	return block_compare(state, block, vblock);
+}
+
+/**
+ * rsu_misc_image_block_init() - initialize state machine for processing blocks
+ * @state: current state machine state
+ *
+ * Function is called before processing images either for writing to flash or
+ * for comparison with verification data.
+ *
+ * Returns 0 on success, or -1 on error
+ */
+static int rsu_misc_image_block_init(struct rsu_image_state *state)
+{
+	rsu_log(RSU_DEBUG, "Resetting image block state machine\n");
+
+	state->no_sections = 1;
+	add_section(state, 0);
+	state->block_type = REGULAR_BLOCK;
+	state->absolute = 0;
+	state->offset = -IMAGE_BLOCK_SZ;
+
+	return 0;
+}
+
+/**
+ * rsu_misc_image_block_process() - process image blocks
+ *
+ * @state: current state machine state
+ * @block: pointer to current 4KB image block
+ * @vblock: pointer to current 4KB image verification block
+ * @info: rsu_slot_info structure for target slot
+ *
+ * Image blocks are processed either for updating before writing to flash
+ * (when vblock==NULL) or for comparison with verification data
+ * (when vblock!=NULL)
+ *
+ * Returns 0 on success and -1 on error
+ */
+static int rsu_misc_image_block_process(struct rsu_image_state *state,
+					void *block, void *vblock,
+					struct rsu_slot_info *info)
+{
+	u32 magic;
+
+	state->offset += IMAGE_BLOCK_SZ;
+
+	if (find_section(state, state->offset))
+		state->block_type = SECTION_BLOCK;
+
+	switch (state->block_type) {
+	case SECTION_BLOCK:
+		magic = *(__u32 *)block;
+		if (magic == CMF_MAGIC) {
+			rsu_log(RSU_DEBUG, "Found CMF sect @0x%08x\n",
+				state->offset);
+			state->block_type = SIGNATURE_BLOCK;
+		} else {
+			state->block_type = REGULAR_BLOCK;
+		}
+
+		if (vblock)
+			return block_compare(state, block, vblock);
+		break;
+
+	case SIGNATURE_BLOCK:
+		rsu_log(RSU_DEBUG, "Found signature block @0x%08x\n",
+			state->offset);
+
+		if (sig_block_process(state, block, info))
+			return -1;
+
+		state->block_type = REGULAR_BLOCK;
+
+		if (vblock)
+			return sig_block_compare(state, block, vblock, info);
+
+		if (sig_block_adjust(state, block, info))
+			return -1;
+
+		break;
+
+	case REGULAR_BLOCK:
+		break;
+	}
+
+	if (vblock)
+		return block_compare(state, block, vblock);
+
+	return 0;
+}
+
+/**
  * rsu_cb_program_common - callback to program flash
  * @ll_intf: pointer to ll_intf
  * @slot: slot number
@@ -344,6 +617,7 @@ int rsu_cb_program_common(struct rsu_ll_intf *ll_intf, int slot,
 	int cnt, c, done;
 	int x;
 	struct rsu_slot_info info;
+	struct rsu_image_state state;
 
 	if (!ll_intf)
 		return -EINTF;
@@ -378,6 +652,9 @@ int rsu_cb_program_common(struct rsu_ll_intf *ll_intf, int slot,
 	offset = 0;
 	done = 0;
 
+	if (rsu_misc_image_block_init(&state))
+		return -EPROGRAM;
+
 	while (!done) {
 		cnt = 0;
 		while (cnt < IMAGE_BLOCK_SZ) {
@@ -394,10 +671,10 @@ int rsu_cb_program_common(struct rsu_ll_intf *ll_intf, int slot,
 		if (cnt == 0)
 			break;
 
-		if (!rawdata && offset == IMAGE_PTR_BLOCK &&
-		    cnt == IMAGE_BLOCK_SZ &&
-		    rsu_misc_image_adjust(buf, &info))
-			return -EPROGRAM;
+		if (!rawdata)
+			if (rsu_misc_image_block_process(&state, buf, NULL,
+							 &info))
+				return -EPROGRAM;
 
 		if ((offset + cnt) > ll_intf->partition.size(part_num)) {
 			rsu_log(RSU_ERR,
@@ -446,9 +723,16 @@ int rsu_cb_verify_common(struct rsu_ll_intf *ll_intf, int slot,
 	unsigned char vbuf[IMAGE_BLOCK_SZ];
 	int cnt, c, done;
 	int x;
+	struct rsu_slot_info info;
+	struct rsu_image_state state;
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (rsu_slot_get_info(slot, &info)) {
+		rsu_log(RSU_ERR, "Unable to read slot info\n");
+		return -ESLOTNUM;
+	}
 
 	part_num = rsu_misc_slot2part(ll_intf, slot);
 	if (part_num < 0)
@@ -464,6 +748,9 @@ int rsu_cb_verify_common(struct rsu_ll_intf *ll_intf, int slot,
 
 	offset = 0;
 	done = 0;
+
+	if (rsu_misc_image_block_init(&state))
+		return -ECMP;
 
 	while (!done) {
 		cnt = 0;
@@ -485,18 +772,23 @@ int rsu_cb_verify_common(struct rsu_ll_intf *ll_intf, int slot,
 		if (ll_intf->data.read(part_num, offset, cnt, vbuf))
 			return -ELOWLEVEL;
 
-		for (x = 0; x < cnt; x++) {
-			if (!rawdata && (offset + x) >= IMAGE_PTR_START &&
-			    (offset + x) <= IMAGE_PTR_END)
-				continue;
+		if (!rawdata) {
+			if (rsu_misc_image_block_process(&state, buf, vbuf,
+							 &info))
+				return -ECMP;
 
+			offset += cnt;
+			continue;
+		}
+
+		for (x = 0; x < cnt; x++)
 			if (vbuf[x] != buf[x]) {
 				rsu_log(RSU_ERR,
-					"Expect %02X, got %02X @ 0x%08X",
+					"Expect %02X, got %02X @ 0x%08X\n",
 					buf[x], vbuf[x], offset + x);
 				return -ECMP;
 			}
-		}
+
 		offset += cnt;
 	}
 
