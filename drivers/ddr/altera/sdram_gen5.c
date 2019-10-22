altera-opensource/u-boot-socfpga-next

ddr: socfpga: Add ECC DRAM scrubbing support for gen5
The SDRAM must first be rewritten by zeroes if ECC is used to initialize
the ECC metadata. Make the CPU overwrite the DRAM with zeroes in such a
case. This scrubbing implementation turns the caches on temporarily, then
overwrites the whole RAM with zeroes, flushes the caches and turns them
off again. This provides satisfactory performance.

Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

drivers/ddr/altera/sdram_gen5.c
@@ -14,8 +14,11 @@
 #include <asm/arch/sdram.h>
 #include <asm/arch/system_manager.h>
 #include <asm/io.h>
+#include <linux/sizes.h>
 
 #include "sequencer.h"
+
+#define PGTABLE_OFF	0x4000
 
 #ifdef CONFIG_SPL_BUILD
 
@@ -557,6 +560,54 @@ static unsigned long sdram_calculate_size(struct socfpga_sdr_ctrl *sdr_ctrl)
 	return temp;
 }
 
+static int sdram_is_ecc_enabled(struct socfpga_sdr_ctrl *sdr_ctrl)
+{
+	return !!(readl(&sdr_ctrl->ctrl_cfg) &
+		  SDR_CTRLGRP_CTRLCFG_ECCEN_MASK);
+}
+
+/* Initialize SDRAM ECC bits to avoid false DBE */
+static void sdram_init_ecc_bits(u32 size)
+{
+	u32 start, size_init, start_addr;
+
+	start = get_timer(0);
+
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size = size;
+
+	gd->arch.tlb_addr = gd->bd->bi_dram[0].start + PGTABLE_OFF;
+	gd->arch.tlb_size = PGTABLE_SIZE;
+
+	memset((void *)gd->bd->bi_dram[0].start, 0, gd->arch.tlb_addr +
+	       gd->arch.tlb_size);
+
+	icache_enable();
+	dcache_enable();
+
+	printf("DDRCAL: Scrubbing ECC RAM (%d MiB).\n", size >> 20);
+
+	start_addr = gd->arch.tlb_addr + gd->arch.tlb_size;
+	size -= (gd->arch.tlb_addr + gd->arch.tlb_size);
+
+	while (size > 0) {
+		size_init = min((phys_addr_t)SZ_1G, (phys_addr_t)size);
+		memset((void *)start_addr, 0, size_init);
+		size -= size_init;
+		start_addr += size_init;
+		WATCHDOG_RESET();
+	}
+
+	flush_dcache_all();
+
+	printf("DDRCAL: Scrubbing ECC RAM done.\n");
+
+	dcache_disable();
+
+	printf("DDRCAL: SDRAM-ECC initialized success with %d ms\n",
+	       (u32)get_timer(start));
+}
+
 static int altera_gen5_sdram_ofdata_to_platdata(struct udevice *dev)
 {
 	struct altera_gen5_sdram_platdata *plat = dev->platdata;
@@ -598,6 +649,10 @@ static int altera_gen5_sdram_probe(struct udevice *dev)
 
 	sdram_size = sdram_calculate_size(sdr_ctrl);
 	debug("SDRAM: %ld MiB\n", sdram_size >> 20);
+
+	if (sdram_is_ecc_enabled(sdr_ctrl)) {
+		sdram_init_ecc_bits(sdram_size);
+	}
 
 	/* Sanity check ensure correct SDRAM size specified */
 	if (get_ram_size(0, sdram_size) != sdram_size) {

altera-opensource/u-boot-socfpga

drivers/ddr/altera/sdram_gen5.c
@@ -14,8 +14,11 @@
 #include <asm/arch/sdram.h>
 #include <asm/arch/system_manager.h>
 #include <asm/io.h>
+#include <linux/sizes.h>
 
 #include "sequencer.h"
+
+#define PGTABLE_OFF	0x4000
 
 #ifdef CONFIG_SPL_BUILD
 
@@ -557,6 +560,54 @@ static unsigned long sdram_calculate_size(struct socfpga_sdr_ctrl *sdr_ctrl)
 	return temp;
 }
 
+static int sdram_is_ecc_enabled(struct socfpga_sdr_ctrl *sdr_ctrl)
+{
+	return !!(readl(&sdr_ctrl->ctrl_cfg) &
+		  SDR_CTRLGRP_CTRLCFG_ECCEN_MASK);
+}
+
+/* Initialize SDRAM ECC bits to avoid false DBE */
+static void sdram_init_ecc_bits(phys_size_t size)
+{
+	phys_size_t start, size_init, start_addr;
+
+	start = get_timer(0);
+
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size = size;
+
+	gd->arch.tlb_addr = gd->bd->bi_dram[0].start + PGTABLE_OFF;
+	gd->arch.tlb_size = PGTABLE_SIZE;
+
+	memset((void *)gd->bd->bi_dram[0].start, 0, gd->arch.tlb_addr +
+	       gd->arch.tlb_size);
+
+	icache_enable();
+	dcache_enable();
+
+	printf("DDRCAL: Scrubbing ECC RAM (%lu MiB).\n", size >> 20);
+
+	start_addr = gd->arch.tlb_addr + gd->arch.tlb_size;
+	size -= (gd->arch.tlb_addr + gd->arch.tlb_size);
+
+	while (size > 0) {
+		size_init = min((phys_addr_t)SZ_1G, (phys_addr_t)size);
+		memset((void *)start_addr, 0, size_init);
+		size -= size_init;
+		start_addr += size_init;
+		WATCHDOG_RESET();
+	}
+
+	flush_dcache_all();
+
+	printf("DDRCAL: Scrubbing ECC RAM done.\n");
+
+	dcache_disable();
+
+	printf("DDRCAL: SDRAM-ECC initialized success with %d ms\n",
+	       (u32)get_timer(start));
+}
+
 static int altera_gen5_sdram_ofdata_to_platdata(struct udevice *dev)
 {
 	struct altera_gen5_sdram_platdata *plat = dev->platdata;
@@ -598,6 +649,10 @@ static int altera_gen5_sdram_probe(struct udevice *dev)
 
 	sdram_size = sdram_calculate_size(sdr_ctrl);
 	debug("SDRAM: %ld MiB\n", sdram_size >> 20);
+
+	if (sdram_is_ecc_enabled(sdr_ctrl)) {
+		sdram_init_ecc_bits(sdram_size);
+	}
 
 	/* Sanity check ensure correct SDRAM size specified */
 	if (get_ram_size(0, sdram_size) != sdram_size) {