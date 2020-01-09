287776b3fc8bd8650a0707d0afea5a4c413e7e34
Chee Hong Ang <chee.hong.ang@intel.com>
2020-01-09 12:32:08 +0800

    HSD #1809587061: arm: socfpga: psci: Fix race condition in CPU_ON
    Adding dsb() between the memory write to ensure each write transaction
    is completed before continuing the next transaction to prevent race
    condition in waking up slave CPUs. This fix ensure all the remaining
    slave CPU cores are being brought up sucessfully by master CPU.
    
    Signed-off-by: Chee Hong Ang <chee.hong.ang@intel.com>

--- arch/arm/mach-socfpga/psci.c
+++ arch/arm/mach-socfpga/psci.c
@@ -148,7 +148,9 @@ int __secure psci_cpu_on_64(u32 function_id, u64 cpuid, u64 entry_point)
 
 	/* to store in global so psci_cpu_on_64_mpidr function can refer */
 	writeq(entry_point, &psci_cpu_on_64_entry_point);
+	dsb();
 	writeq(cpuid, &psci_cpu_on_64_cpuid);
+	dsb();
 
 	asm volatile("sev");
 
