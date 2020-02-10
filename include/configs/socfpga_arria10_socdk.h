295cf2dc2dde7199bb2b5e9e543895c79d745bd1
Tien Fong Chee <tien.fong.chee@intel.com>
2020-02-10 13:12:40 +0800

    ARM: socfpga: Synchronize the configuration for A10 QSPI SoCDK
    Update the default configuration file to enable the necessary
    functionality the get the QSPI SoCDK working. That includes fitImage
    loading in SPL, booting both U-Boot and Linux.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

--- include/configs/socfpga_arria10_socdk.h
+++ include/configs/socfpga_arria10_socdk.h
@@ -8,6 +8,12 @@
 
 #include <asm/arch/base_addr_a10.h>
 
+/* Booting Linux */
+#define CONFIG_LOADADDR		0x01000000
+#define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
+
+#define CONFIG_SYS_BOOTM_LEN	(32 * 1024 * 1024)
+
 /*
  * U-Boot general configurations
  */
@@ -31,6 +37,27 @@
  */
 
 /* SPL memory allocation configuration, this is for FAT implementation */
+
+#if defined(CONFIG_QSPI_BOOT)
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"bootm_size=0xa000000\0" \
+	"kernel_addr_r="__stringify(CONFIG_SYS_LOAD_ADDR)"\0" \
+	"fdt_addr_r=0x02000000\0" \
+	"scriptaddr=0x02100000\0" \
+	"pxefile_addr_r=0x02200000\0" \
+	"ramdisk_addr_r=0x02300000\0" \
+	"socfpga_legacy_reset_compat=1\0" \
+	"kernelfit_addr=0x1200000\0" \
+	"fitimagesize=0x5F0000\0" \
+	"qspiroot=/dev/mtdblock1\0" \
+	"qspirootfstype=jffs2\0" \
+	"qspiload=sf probe; sf read ${scriptaddr} ${kernelfit_addr}\0" \
+	"qspiboot=setenv bootargs " CONFIG_BOOTARGS \
+			"root=${qspiroot} rw rootfstype=${qspirootfstype}; " \
+			"bootm ${scriptaddr}\0" \
+
+#endif
 
 /* The rest of the configuration is shared */
 #include <configs/socfpga_common.h>
