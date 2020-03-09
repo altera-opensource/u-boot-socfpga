46279763d94aee5a102901a7fd46944fb6a294f5
Ley Foon Tan <ley.foon.tan@intel.com>
2020-03-09 14:28:49 +0800

    HSD #1707151571: configs: socfpga: Add QSPI support for Cyclone 5
    Add QSPI boot support to boot target devices list.
    Platform can provide their own boot settings through SOCFPGA_BOOT_SETTINGS
    macro if needed.
    
    Add SOCFPGA_BOOT_SETTINGS for Cyclone 5.
    
    Signed-off-by: Ley Foon Tan <ley.foon.tan@intel.com>

--- include/configs/socfpga_cyclone5_socdk.h
+++ include/configs/socfpga_cyclone5_socdk.h
@@ -10,6 +10,28 @@
 /* Memory configurations */
 #define PHYS_SDRAM_1_SIZE		0x40000000	/* 1GiB on SoCDK */
 
+/* Booting Linux */
+#define CONFIG_LOADADDR		0x01000000
+#define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
+
+/* QSPI boot */
+#define FDT_SIZE		__stringify(0x00010000)
+#define KERNEL_SIZE		__stringify(0x005d0000)
+#define QSPI_FDT_ADDR		__stringify(0x00220000)
+#define QSPI_KERNEL_ADDR	__stringify(0x00230000)
+
+#define SOCFPGA_BOOT_SETTINGS \
+	"fdt_size=" FDT_SIZE "\0" \
+	"kernel_size=" KERNEL_SIZE "\0" \
+	"qspi_fdt_addr=" QSPI_FDT_ADDR "\0" \
+	"qspi_kernel_addr=" QSPI_KERNEL_ADDR "\0" \
+	"qspiboot=setenv bootargs earlycon " \
+		"root=/dev/mtdblock1 rw rootfstype=jffs2; " \
+		"bootz ${kernel_addr_r} - ${fdt_addr_r}\0" \
+	"qspiload=sf probe; " \
+		"sf read ${kernel_addr_r} ${qspi_kernel_addr} ${kernel_size}; " \
+		"sf read ${fdt_addr_r} ${qspi_fdt_addr} ${fdt_size}\0"
+
 /* The rest of the configuration is shared */
 #include <configs/socfpga_common.h>
 
