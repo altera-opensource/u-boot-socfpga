dbd3899c92ab9f74fd9a23b49ccc6530bd4ab801
Chee Hong Ang <chee.hong.ang@intel.com>
2020-04-14 18:30:10 +0800

    ARM: socfpga: stratix10: Add SOCFPGA bridges reset support for PSCI call
    Add SOCFPGA bridges reset support for FPGA configuration SMC services
    to disable/enable the bridges before and after the FPGA configuration
    process.
    
    Signed-off-by: Chee Hong Ang <chee.hong.ang@intel.com>

--- arch/arm/mach-socfpga/include/mach/reset_manager_soc64.h
+++ arch/arm/mach-socfpga/include/mach/reset_manager_soc64.h
@@ -10,6 +10,7 @@ void reset_deassert_peripherals_handoff(void);
 int cpu_has_been_warmreset(void);
 void print_reset_info(void);
 void socfpga_bridges_reset(int enable);
+void socfpga_bridges_reset_psci(int enable);
 
 #define RSTMGR_SOC64_STATUS	0x00
 #define RSTMGR_SOC64_MPUMODRST	0x20
