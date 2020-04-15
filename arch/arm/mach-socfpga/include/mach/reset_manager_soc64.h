adb14f122e2af7bdba89204da8cc710340df2e87
Thor Thayer <thor.thayer@linux.intel.com>
2020-04-15 15:13:30 +0800

    HSD #1806784746-2: S10: Add support for RESET2 to PSCI
    [backport 'commit fec8032ba038 ("HSD #1806784746-2: S10: Add
    				 support for RESET2 to PSCI")']
    Backport from uboot-socfpga/socfpga_v2017.09
    
    Add Warm Reset support using RESET2 for PSCI to support warm
    reboot. Linux uses the RESET2 mechanism for warm reset
    and panic.
    
    Add a new secure l2_reset_cpu_psci() for PSCI. The original
    l2_reset can't be called from SMC context which is what Linux uses.
    
    Signed-off-by: Thor Thayer <thor.thayer@linux.intel.com>

--- arch/arm/mach-socfpga/include/mach/reset_manager_soc64.h
+++ arch/arm/mach-socfpga/include/mach/reset_manager_soc64.h
@@ -12,6 +12,7 @@ int cpu_has_been_warmreset(void);
 void print_reset_info(void);
 void socfpga_bridges_reset(int enable, unsigned int mask);
 void socfpga_bridges_reset_psci(int enable);
+void l2_reset_cpu_psci(void);
 
 #define RSTMGR_SOC64_STATUS	0x00
 #define RSTMGR_SOC64_HDSKEN	0x10
