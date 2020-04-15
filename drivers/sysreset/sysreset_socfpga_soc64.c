b817269cec17943fe748a367692531dc88165df0
Thor Thayer <thor.thayer@linux.intel.com>
2020-04-15 15:13:30 +0800

    HSD #1806784746-2: S10: Add support for RESET2 to PSCI
    [backport 'commit fec8032ba038 ("HSD #1806784746-2: S10: Add
    				 support for RESET2 to PSCI")']
    Backport from uboot-socfpga/socfpga_v2017.09
    
    Add Warm Reset support using RESET2 for PSCI to support warm
    reboot. Linux uses the RESET2 mechanism for warm reset
    and panic.
    
    Add a new secure l2_reset_cpu_psci() for PSCI. The original
    l2_reset can't be called from SMC context which is what Linux uses.
    
    Signed-off-by: Thor Thayer <thor.thayer@linux.intel.com>

--- drivers/sysreset/sysreset_socfpga_soc64.c
+++ drivers/sysreset/sysreset_socfpga_soc64.c
@@ -78,6 +78,44 @@ void l2_reset_cpu(void)
 		: "x1", "x2", "x3");
 }
 
+void __secure l2_reset_cpu_psci(void)
+{
+	asm volatile(
+		/* Disable GIC distributor (IRQs). */
+		"str    wzr, [%3]\n"
+		/* Set Magic Number */
+		"str	%0, [%1]\n"
+		/* Increase timeout in rstmgr.hdsktimeout */
+		"ldr	x2, =0xFFFFFF\n"
+		"str	w2, [%2, #0x64]\n"
+		"ldr	w2, [%2, #0x10]\n"
+		/*
+		 * Set l2flushen = 1, etrstallen = 1,
+		 * fpgahsen = 1 and sdrselfrefen = 1
+		 * in rstmgr.hdsken to perform handshake
+		 * in certain peripherals before trigger
+		 * L2 reset.
+		 */
+		"ldr	x3, =0x10D\n"
+		"orr	x2, x2, x3\n"
+		"str	w2, [%2, #0x10]\n"
+		/* Trigger L2 reset in rstmgr.coldmodrst */
+		"ldr	w2, [%2, #0x34]\n"
+		"orr	x2, x2, #0x100\n"
+		"isb\n"
+		"dsb	sy\n"
+		"str	w2, [%2, #0x34]\n"
+		/* Put all cores into WFI mode */
+		"wfi_loop2:\n"
+		"	wfi\n"
+		"	b	wfi_loop2\n"
+		: : "r" (L2_RESET_DONE_STATUS),
+		    "r" (L2_RESET_DONE_REG),
+		    "r" (SOCFPGA_RSTMGR_ADDRESS),
+		    "r" (GICD_CTRL_ADDRESS)
+		: "x1", "x2", "x3");
+}
+
 static struct sysreset_ops socfpga_sysreset = {
 	.request = socfpga_sysreset_request,
 };
