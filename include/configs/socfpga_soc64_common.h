0c2990426d3268b9bf90ce22e10dfe606020286e
Ley Foon Tan <ley.foon.tan@intel.com>
2020-04-16 01:11:43 +0800

    HSD #1507526426-10: configs: socfpga: agilex: Add NAND boot support
    Add NAND boot env settings
    
    Signed-off-by: Ley Foon Tan <ley.foon.tan@intel.com>

--- include/configs/socfpga_soc64_common.h
+++ include/configs/socfpga_soc64_common.h
@@ -138,6 +138,11 @@ unsigned int cm_get_qspi_controller_clk_hz(void);
 	"scriptfile=u-boot.scr\0" \
 	"fatscript=if fatload mmc 0:1 ${scriptaddr} ${scriptfile};" \
 		   "then source ${scriptaddr}; fi\0" \
+	"nandroot=/dev/mtdblock5\0" \
+	"nandload=nand read ${loadaddr} kernel; nand read ${fdt_addr} dtb\0" \
+	"nandboot=setenv bootargs " CONFIG_BOOTARGS \
+			" root=${nandroot} rw rootwait rootfstype=jffs2; " \
+			"booti ${loadaddr} - ${fdt_addr}\0" \
 	"socfpga_legacy_reset_compat=1\0"
 
 /*
