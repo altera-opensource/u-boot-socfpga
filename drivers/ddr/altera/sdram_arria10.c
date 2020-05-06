573e342d802edf07a308ceae85c070bda170498b
Ley Foon Tan <ley.foon.tan@intel.com>
2020-05-06 11:14:49 +0800

    HSD #1507976624-1: ddr: altera: arria10: Move common code to common file
    Move common code sdram_init_ecc_bits() to new common file sdram_soc32.c.
    Preparation for Gen5 uses the same ECC scrubbing function.
    
    Signed-off-by: Ley Foon Tan <ley.foon.tan@intel.com>

--- drivers/ddr/altera/sdram_arria10.c
+++ drivers/ddr/altera/sdram_arria10.c
@@ -4,7 +4,6 @@
  */
 
 #include <common.h>
-#include <cpu_func.h>
 #include <errno.h>
 #include <fdtdec.h>
 #include <init.h>
@@ -24,6 +23,8 @@
 #include <linux/delay.h>
 #include <linux/kernel.h>
 #include <linux/sizes.h>
+
+#include "sdram_soc32.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -197,57 +198,6 @@ static int sdram_is_ecc_enabled(void)
 {
 	return !!(readl(&socfpga_ecc_hmc_base->eccctrl) &
 		  ALT_ECC_HMC_OCP_ECCCTL_ECC_EN_SET_MSK);
-}
-
-static void sdram_init_ecc_bits(void)
-{
-	u32 start;
-	phys_addr_t start_addr, saved_start;
-	phys_size_t size, size_init, saved_size;
-#if defined(CONFIG_SYS_ARM_CACHE_WRITETHROUGH)
-	enum dcache_option option = DCACHE_WRITETHROUGH;
-#elif defined(CONFIG_SYS_ARM_CACHE_WRITEALLOC)
-	enum dcache_option option = DCACHE_WRITEALLOC;
-#else
-	enum dcache_option option = DCACHE_WRITEBACK;
-#endif
-
-	start = get_timer(0);
-
-	start_addr = gd->bd->bi_dram[0].start;
-	size = gd->bd->bi_dram[0].size;
-
-	printf("DDRCAL: Scrubbing ECC RAM (%ld MiB).\n", size >> 20);
-
-	memset((void *)start_addr, 0, PGTABLE_SIZE + PGTABLE_OFF);
-	gd->arch.tlb_addr = start_addr + PGTABLE_OFF;
-	gd->arch.tlb_size = PGTABLE_SIZE;
-	start_addr += PGTABLE_SIZE + PGTABLE_OFF;
-	size -= (PGTABLE_OFF + PGTABLE_SIZE);
-	dcache_enable();
-
-	saved_start = start_addr;
-	saved_size = size;
-	/* Set SDRAM region to writethrough to avoid false double-bit error. */
-	mmu_set_region_dcache_behaviour(saved_start, saved_size,
-					DCACHE_WRITETHROUGH);
-
-	while (size > 0) {
-		size_init = min((phys_addr_t)SZ_1G, (phys_addr_t)size);
-		memset((void *)start_addr, 0, size_init);
-		size -= size_init;
-		start_addr += size_init;
-		WATCHDOG_RESET();
-	}
-
-	dcache_disable();
-
-	/* Restore back to original dcache behaviour. */
-	mmu_set_region_dcache_behaviour(saved_start, saved_size,
-					option);
-
-	printf("DDRCAL: SDRAM-ECC initialized success with %d ms\n",
-	       (u32)get_timer(start));
 }
 
 /* Function to startup the SDRAM*/
---
+++ drivers/ddr/altera/sdram_soc32.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 Intel Corporation <www.intel.com>
+ */
+
+#ifndef	_SDRAM_SOC32_H_
+#define	_SDRAM_SOC32_H_
+
+void sdram_init_ecc_bits(void);
+
+#endif /* _SDRAM_SOC32_H_ */
