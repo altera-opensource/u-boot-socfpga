3ee17dbfbdf253ce50d5ddfaf5485c7b65683baa
Ley Foon Tan <ley.foon.tan@intel.com>
2020-05-20 14:50:25 +0800

    HSD #1307580466-2: arm: socfpga: mailbox: Add mailbox retry support
    [Backport from v2019.10 commit ID 9e299b430b10]
    
    Resend mailbox command for 3 times with 2ms interval in between if
    it receives MBOX_RESP_TIMEOUT and MBOX_RESP_DEVICE_BUSY response code.
    
    Add a wrapper function __mbox_send_cmd_retry() for retry, change
    all the callers to use this wrapper function.
    
    Signed-off-by: Ley Foon Tan <ley.foon.tan@intel.com>

--- arch/arm/mach-socfpga/mailbox_s10.c
+++ arch/arm/mach-socfpga/mailbox_s10.c
@@ -309,6 +309,25 @@ static __always_inline int __mbox_send_cmd(u8 id, u32 cmd, u8 is_indirect,
 	return -EIO;
 }
 
+static __always_inline int __mbox_send_cmd_retry(u8 id, u32 cmd, u8 is_indirect,
+					   u32 len, u32 *arg, u8 urgent,
+					   u32 *resp_buf_len, u32 *resp_buf)
+{
+	int ret;
+	int i;
+
+	for (i = 0; i < 3; i++) {
+		ret = __mbox_send_cmd(id, cmd, is_indirect, len, arg,
+					   urgent, resp_buf_len, resp_buf);
+		if (ret == MBOX_RESP_TIMEOUT || ret == MBOX_RESP_DEVICE_BUSY)
+			__udelay(2000); /* wait for 2ms before resend */
+		else
+			break;
+	}
+
+	return ret;
+}
+
 int mbox_init(void)
 {
 	static const struct socfpga_mailbox *mbox_base =
@@ -415,7 +434,7 @@ static __always_inline int __mbox_get_fpga_config_status(u32 cmd)
 	int ret;
 
 	reconfig_status_resp_len = RECONFIG_STATUS_RESPONSE_LEN;
-	ret = __mbox_send_cmd(MBOX_ID_UBOOT, cmd,
+	ret = __mbox_send_cmd_retry(MBOX_ID_UBOOT, cmd,
 			      MBOX_CMD_DIRECT, 0, NULL, 0,
 			      &reconfig_status_resp_len,
 			      reconfig_status_resp);
@@ -448,7 +467,7 @@ static __always_inline int __mbox_get_fpga_config_status(u32 cmd)
 int mbox_send_cmd(u8 id, u32 cmd, u8 is_indirect, u32 len, u32 *arg,
 		  u8 urgent, u32 *resp_buf_len, u32 *resp_buf)
 {
-	return __mbox_send_cmd(id, cmd, is_indirect, len, arg, urgent,
+	return __mbox_send_cmd_retry(id, cmd, is_indirect, len, arg, urgent,
 			       resp_buf_len, resp_buf);
 }
 
@@ -456,7 +475,7 @@ int __secure mbox_send_cmd_psci(u8 id, u32 cmd, u8 is_indirect, u32 len,
 				u32 *arg, u8 urgent, u32 *resp_buf_len,
 				u32 *resp_buf)
 {
-	return __mbox_send_cmd(id, cmd, is_indirect, len, arg, urgent,
+	return __mbox_send_cmd_retry(id, cmd, is_indirect, len, arg, urgent,
 			       resp_buf_len, resp_buf);
 }
 
