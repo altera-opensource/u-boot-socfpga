db0574851fe3c885d0c9a6e9abd97dcfa3641fc5
Richard Gong <richard.gong@intel.com>
2020-08-24 11:17:47 -0500

    HSD #22010730357: rsu: changes for Sub-Partition Table (SPT) enhancements
    Changes are including:
    1). integrity check
            a). check SPT header fields
            b). check SPT data fields
            c). check bot SPTs if both are same
    2). new command and RSU core extension to support
            a). rsu restore_spt <address> - restore a saved SPT from address
            b). rsu save_spt <address> - save SPT to address
    
    If one of SPTs is corrupted, update the corrupted SPT with the good one.
    
    If both SPTs are corrupted, all SPT related operations are blocked until
    user successfully run "rsu restore_spt <address>".
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>

--- arch/arm/mach-socfpga/rsu.c
+++ arch/arm/mach-socfpga/rsu.c
@@ -57,6 +57,15 @@ void rsu_exit(void)
 }
 
 /**
+ * rsu_spt_corrupted_info() - output warning info to user
+ */
+void rsu_spt_corrupted_info(void)
+{
+	rsu_log(RSU_ERR, "corrupted SPT --");
+	rsu_log(RSU_ERR, "run rsu restore_spt <address> first\n");
+}
+
+/**
  * rsu_cpb_corrupted_info() - output warning info to user
  */
 void rsu_cpb_corrupted_info(void)
@@ -79,6 +88,11 @@ int rsu_slot_count(void)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	partitions = ll_intf->partition.count();
 
@@ -104,6 +118,11 @@ int rsu_slot_by_name(char *name)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (!name)
 		return -EARGS;
@@ -137,6 +156,11 @@ int rsu_slot_get_info(int slot, struct rsu_slot_info *info)
 
 	if (!info)
 		return -EARGS;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (ll_intf->cpb_ops.corrupted()) {
 		rsu_cpb_corrupted_info();
@@ -176,6 +200,11 @@ int rsu_slot_size(int slot)
 	if (!ll_intf)
 		return -EINTF;
 
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
+
 	if (slot < 0 || slot >= rsu_slot_count()) {
 		rsu_log(RSU_ERR, "invalid slot number\n");
 		return -ESLOTNUM;
@@ -204,9 +233,19 @@ int rsu_slot_priority(int slot)
 	if (!ll_intf)
 		return -EINTF;
 
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
+
 	if (ll_intf->cpb_ops.corrupted()) {
 		rsu_cpb_corrupted_info();
 		return -ECORRUPTED_CPB;
+	}
+
+	if (slot < 0 || slot >= rsu_slot_count()) {
+		rsu_log(RSU_ERR, "invalid slot number\n");
+		return -ESLOTNUM;
 	}
 
 	if (slot < 0 || slot >= rsu_slot_count()) {
@@ -236,6 +275,11 @@ int rsu_slot_erase(int slot)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (ll_intf->cpb_ops.corrupted()) {
 		rsu_cpb_corrupted_info();
@@ -287,6 +331,11 @@ int rsu_slot_program_buf(int slot, void *buf, int size)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (ll_intf->cpb_ops.corrupted()) {
 		rsu_cpb_corrupted_info();
@@ -343,6 +392,11 @@ int rsu_slot_program_buf_raw(int slot, void *buf, int size)
 	if (!ll_intf)
 		return -EINTF;
 
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
+
 	if (slot < 0 || slot >= rsu_slot_count()) {
 		rsu_log(RSU_ERR, "invalid slot number\n");
 		return -ESLOTNUM;
@@ -380,6 +434,11 @@ int rsu_slot_verify_buf(int slot, void *buf, int size)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (ll_intf->cpb_ops.corrupted()) {
 		rsu_cpb_corrupted_info();
@@ -425,6 +484,11 @@ int rsu_slot_verify_buf_raw(int slot, void *buf, int size)
 	if (!ll_intf)
 		return -EINTF;
 
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
+
 	if (slot < 0 || slot >= rsu_slot_count()) {
 		rsu_log(RSU_ERR, "invalid slot number\n");
 		return -ESLOTNUM;
@@ -460,6 +524,11 @@ int rsu_slot_enable(int slot)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (ll_intf->cpb_ops.corrupted()) {
 		rsu_cpb_corrupted_info();
@@ -499,6 +568,11 @@ int rsu_slot_disable(int slot)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (ll_intf->cpb_ops.corrupted()) {
 		rsu_cpb_corrupted_info();
@@ -584,6 +658,11 @@ int rsu_slot_load_factory(void)
 	if (!ll_intf)
 		return -EINTF;
 
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
+
 	partitions = ll_intf->partition.count();
 	for (part_num = 0; part_num < partitions; part_num++) {
 		if (!strcmp(name, ll_intf->partition.name(part_num)))
@@ -612,6 +691,11 @@ int rsu_slot_rename(int slot, char *name)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (slot < 0 || slot >= rsu_slot_count()) {
 		rsu_log(RSU_ERR, "invalid slot number\n");
@@ -648,6 +732,11 @@ int rsu_slot_delete(int slot)
 
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (ll_intf->cpb_ops.corrupted()) {
 		rsu_cpb_corrupted_info();
@@ -692,6 +781,11 @@ int rsu_slot_create(char *name, u64 address, unsigned int size)
 {
 	if (!ll_intf)
 		return -EINTF;
+
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
 
 	if (rsu_misc_is_rsvd_name(name)) {
 		rsu_log(RSU_ERR, "Partition create uses a reserved name\n");
@@ -986,4 +1080,35 @@ int rsu_save_cpb(u64 address)
 	}
 
 	return ll_intf->cpb_ops.save(address);
+}
+
+/**
+ * rsu_restore_spt() - restore the spt from an address
+ * @address: the address which spt will be restored from.
+ *
+ * This function is used to restore a saved SPT from an address.
+ *
+ * Returns: 0 on success, or error code
+ */
+int rsu_restore_spt(u64 address)
+{
+	return ll_intf->spt_ops.restore(address);
+}
+
+/**
+ * rsu_save_spt() - save spt to the address
+ * @address: the address which spt will be saved to.
+ *
+ * This function is used to save SPT to an address.
+ *
+ * Returns: 0 on success, or error code
+ */
+int rsu_save_spt(u64 address)
+{
+	if (ll_intf->spt_ops.corrupted()) {
+		rsu_spt_corrupted_info();
+		return -ECORRUPTED_SPT;
+	}
+
+	return ll_intf->spt_ops.save(address);
 }
