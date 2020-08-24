db131518e1f7cbf00fc08bf3bd641327cd942d2d
Richard Gong <richard.gong@intel.com>
2020-08-24 11:17:47 -0500

    HSD #22010730357: rsu: changes for Sub-Partition Table (SPT) enhancements
    Changes are including:
    1). integrity check
            a). check SPT header fields
            b). check SPT data fields
            c). check bot SPTs if both are same
    2). new command and RSU core extension to support
            a). rsu restore_spt <address> - restore a saved SPT from address
            b). rsu save_spt <address> - save SPT to address
    
    If one of SPTs is corrupted, update the corrupted SPT with the good one.
    
    If both SPTs are corrupted, all SPT related operations are blocked until
    user successfully run "rsu restore_spt <address>".
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>

--- arch/arm/mach-socfpga/rsu_s10.c
+++ arch/arm/mach-socfpga/rsu_s10.c
@@ -990,6 +990,54 @@ static int save_cpb(int argc, char * const argv[])
 	return CMD_RET_SUCCESS;
 }
 
+static int restore_spt(int argc, char * const argv[])
+{
+	int ret;
+	u64 addr;
+	char *endp;
+
+	if (argc != 2)
+		return CMD_RET_USAGE;
+
+	if (!initialized) {
+		if (rsu_init(NULL))
+			return CMD_RET_FAILURE;
+
+		initialized = 1;
+	}
+
+	addr = simple_strtoul(argv[1], &endp, 16);
+	ret = rsu_restore_spt(addr);
+	if (ret < 0)
+		return CMD_RET_FAILURE;
+
+	return CMD_RET_SUCCESS;
+}
+
+static int save_spt(int argc, char * const argv[])
+{
+	int ret;
+	u64 addr;
+	char *endp;
+
+	if (argc != 2)
+		return CMD_RET_USAGE;
+
+	if (!initialized) {
+		if (rsu_init(NULL))
+			return CMD_RET_FAILURE;
+
+		initialized = 1;
+	}
+
+	addr =  simple_strtoul(argv[1], &endp, 16);
+	ret = rsu_save_spt(addr);
+	if (ret < 0)
+		return CMD_RET_FAILURE;
+
+	return CMD_RET_SUCCESS;
+}
+
 struct func_t {
 	const char *cmd_string;
 	int (*func_ptr)(int cmd_argc, char * const cmd_argv[]);
@@ -1024,6 +1072,8 @@ static const struct func_t rsu_func_t[] = {
 	{"display_dcmf_version", display_dcmf_version},
 	{"display_dcmf_status", display_dcmf_status},
 	{"display_max_retry", display_max_retry},
+	{"save_spt", save_spt},
+	{"restore_spt", restore_spt},
 	{"create_empty_cpb", create_empty_cpb},
 	{"restore_cpb", restore_cpb},
 	{"save_cpb", save_cpb}
@@ -1084,6 +1134,8 @@ U_BOOT_CMD(
 	"display_dcmf_version - display DCMF versions and store them for SMC handler usage\n"
 	"display_dcmf_status - display DCMF status and store it for SMC handler usage\n"
 	"display_max_retry - display max_retry parameter, and store it for SMC handler usage\n"
+	"restore_spt <address> - restore SPT from an address\n"
+	"save_spt <address> - save SPT to an address\n"
 	"create_empty_cpb - create a empty CPB\n"
 	"restore_cpb <address> - restore CPB from an address\n"
 	"save_cpb <address> - save CPB to an address\n"
