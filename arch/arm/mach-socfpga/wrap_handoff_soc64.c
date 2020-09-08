0c840bc5214ff883baed8f5d981ef4ad63b41dce
Siew Chin Lim <elly.siew.chin.lim@intel.com>
2020-09-08 18:00:39 +0800

    arm: socfpga: Restructure Stratix10 and Agilex handoff code
    Restructure Stratix10 and Agilex handoff code. Rename _s10.c file
    to _soc64.c file. Move common code that parsing the handoff data
    to wrap_handoff_soc64.c, in preparation to support handoff for
    Diamond Mesa.
    
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

---
+++ arch/arm/mach-socfpga/wrap_handoff_soc64.c
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2020 Intel Corporation <www.intel.com>
+ *
+ */
+
+#include <common.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <asm/arch/handoff_soc64.h>
+#include "log.h"
+
+int get_handoff_size(void *handoff_address, enum endianness endian)
+{
+	u32 handoff_size;
+
+	if (endian == little_endian) {
+		handoff_size = (readl(handoff_address +
+				SOC64_HANDOFF_OFFSET_LENGTH) -
+				SOC64_HANDOFF_OFFSET_DATA) /
+				sizeof(u32);
+	} else if (endian == big_endian) {
+		handoff_size = swab32(readl(handoff_address +
+					    SOC64_HANDOFF_OFFSET_LENGTH));
+		handoff_size = (handoff_size - SOC64_HANDOFF_OFFSET_DATA) /
+				sizeof(u32);
+	} else {
+		return -EINVAL;
+	}
+
+	debug("%s: handoff address = 0x%p handoff size = 0x%08x\n", __func__,
+	      (u32 *)handoff_address, handoff_size);
+
+	return handoff_size;
+}
+
+int handoff_read(void *handoff_address, void *table, u32 table_len,
+		 enum endianness big_endian)
+{
+	u32 temp, i;
+	u32 *table_x32 = table;
+
+	debug("%s: handoff addr = 0x%p ", __func__, (u32 *)handoff_address);
+
+	if (big_endian) {
+		if (swab32(readl(SOC64_HANDOFF_BASE)) ==
+			SOC64_HANDOFF_MAGIC_BOOT) {
+			debug("Handoff table address = 0x%p ", table_x32);
+			debug("table length = 0x%x\n", table_len);
+			debug("%s: handoff data =\n{\n", __func__);
+
+			for (i = 0; i < table_len; i++) {
+				temp = readl(handoff_address +
+					     SOC64_HANDOFF_OFFSET_DATA +
+					     (i * sizeof(u32)));
+				*table_x32 = swab32(temp);
+
+				if (!(i % 2))
+					debug(" No.%d Addr 0x%08x: ", i,
+					      *table_x32);
+				else
+					debug(" 0x%08x\n", *table_x32);
+
+				table_x32++;
+			}
+			debug("\n}\n");
+		} else {
+			debug("%s: Cannot find SOC64_HANDOFF_MAGIC_BOOT ",
+			      __func__);
+			debug("at addr  0x%p\n",
+			      (u32 *)handoff_address);
+			return -EPERM;
+		}
+	}
+
+	return 0;
+}
