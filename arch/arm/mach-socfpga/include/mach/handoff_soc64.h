66280c5c114e77e8fc6aa7552d843c3b81f11b72
Siew Chin Lim <elly.siew.chin.lim@intel.com>
2020-09-08 18:00:39 +0800

    arm: socfpga: Restructure Stratix10 and Agilex handoff code
    Restructure Stratix10 and Agilex handoff code. Rename _s10.c file
    to _soc64.c file. Move common code that parsing the handoff data
    to wrap_handoff_soc64.c, in preparation to support handoff for
    Diamond Mesa.
    
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- arch/arm/mach-socfpga/include/mach/handoff_soc64.h
+++ arch/arm/mach-socfpga/include/mach/handoff_soc64.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0
  *
- * Copyright (C) 2016-2018 Intel Corporation <www.intel.com>
+ * Copyright (C) 2016-2020 Intel Corporation <www.intel.com>
  *
  */
 
@@ -10,6 +10,20 @@
 /*
  * Offset for HW handoff from Quartus tools
  */
+
+/* HPS handoff */
+#define SOC64_HANDOFF_MAGIC_BOOT	0x424F4F54
+#define SOC64_HANDOFF_MAGIC_MUX		0x504D5558
+#define SOC64_HANDOFF_MAGIC_IOCTL	0x494F4354
+#define SOC64_HANDOFF_MAGIC_FPGA	0x46504741
+#define SOC64_HANDOFF_MAGIC_DELAY	0x444C4159
+#define SOC64_HANDOFF_MAGIC_CLOCK	0x434C4B53
+#define SOC64_HANDOFF_MAGIC_MISC	0x4D495343
+
+#define SOC64_HANDOFF_OFFSET_LENGTH	0x4
+#define SOC64_HANDOFF_OFFSET_DATA	0x10
+#define SOC64_HANDOFF_SIZE		4096
+
 #define SOC64_HANDOFF_BASE		0xFFE3F000
 #define SOC64_HANDOFF_MUX		(SOC64_HANDOFF_BASE + 0x10)
 #define SOC64_HANDOFF_IOCTL		(SOC64_HANDOFF_BASE + 0x1A0)
@@ -17,14 +31,6 @@
 #define SOC64_HANDOFF_DELAY		(SOC64_HANDOFF_BASE + 0x3F0)
 #define SOC64_HANDOFF_CLOCK		(SOC64_HANDOFF_BASE + 0x580)
 #define SOC64_HANDOFF_MISC		(SOC64_HANDOFF_BASE + 0x610)
-#define SOC64_HANDOFF_MAGIC_MUX		0x504D5558
-#define SOC64_HANDOFF_MAGIC_IOCTL	0x494F4354
-#define SOC64_HANDOFF_MAGIC_FPGA	0x46504741
-#define SOC64_HANDOFF_MAGIC_DELAY	0x444C4159
-#define SOC64_HANDOFF_MAGIC_CLOCK	0x434C4B53
-#define SOC64_HANDOFF_MAGIC_MISC	0x4D495343
-#define SOC64_HANDOFF_OFFSET_LENGTH	0x4
-#define SOC64_HANDOFF_OFFSET_DATA	0x10
 
 #ifdef CONFIG_TARGET_SOCFPGA_STRATIX10
 #define SOC64_HANDOFF_CLOCK_OSC		(SOC64_HANDOFF_BASE + 0x608)
@@ -34,6 +40,24 @@
 #define SOC64_HANDOFF_CLOCK_FPGA	(SOC64_HANDOFF_BASE + 0x600)
 #endif
 
-#define SOC64_HANDOFF_SIZE		4096
+#define SOC64_HANDOFF_MUX_LEN		96
+#define SOC64_HANDOFF_IOCTL_LEN		96
+#ifdef CONFIG_TARGET_SOCFPGA_STRATIX10
+#define SOC64_HANDOFF_FPGA_LEN		42
+#else
+#define SOC64_HANDOFF_FPGA_LEN		40
+#endif
+#define SOC64_HANDOFF_DELAY_LEN		96
 
+#ifndef __ASSEMBLY__
+#include <asm/types.h>
+enum endianness {
+	little_endian,
+	big_endian
+};
+
+int get_handoff_size(void *handoff_address, enum endianness endian);
+int handoff_read(void *handoff_address, void *table, u32 table_len,
+		 enum endianness big_endian);
+#endif
 #endif /* _HANDOFF_SOC64_H_ */
