4c837a3b539ec3ad0d893c3e4d8ab355b6fb88a7
Siew Chin Lim <elly.siew.chin.lim@intel.com>
2020-09-08 17:08:41 +0800

    arm: socfpga: Rename Stratix10 and Agilex handoff common macros
    Rename handoff_s10.h to handoff_soc64.h. Changed macros prefix from
    S10_HANDOFF to SOC64_HANDOFF.
    
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- arch/arm/mach-socfpga/clock_manager_s10.c
+++ arch/arm/mach-socfpga/clock_manager_s10.c
@@ -7,7 +7,7 @@
 #include <common.h>
 #include <asm/io.h>
 #include <asm/arch/clock_manager.h>
-#include <asm/arch/handoff_s10.h>
+#include <asm/arch/handoff_soc64.h>
 #include <asm/arch/system_manager.h>
 
 DECLARE_GLOBAL_DATA_PTR;
