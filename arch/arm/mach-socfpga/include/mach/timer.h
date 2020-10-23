1b57c65c1d70d5869b164df04016486eb367990a
Siew Chin Lim <elly.siew.chin.lim@intel.com>
2020-10-23 23:10:06 +0800

    HSD #14012726917: Replaced __udelay inline function with __socfpga_udelay inline function
    The udelay function is used by both uboot proper and PSCI code
    for timeout handling.The __udelay inline function was not included
    in '__secure' section which causes random error when called by
    PSCI code that run in '__secure' section.
    
    The solution is replaced the __udelay inline function with
    __socfpga_udelay inline function in timer.h, so that the code
    can be included in '__secure' section.
    
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- arch/arm/mach-socfpga/include/mach/timer.h
+++ arch/arm/mach-socfpga/include/mach/timer.h
@@ -6,6 +6,9 @@
 #ifndef _SOCFPGA_TIMER_H_
 #define _SOCFPGA_TIMER_H_
 
+#include <asm/barriers.h>
+#include <div64.h>
+
 struct socfpga_timer {
 	u32	load_val;
 	u32	curr_val;
@@ -13,5 +16,33 @@ struct socfpga_timer {
 	u32	eoi;
 	u32	int_stat;
 };
+
+static __always_inline u64 __socfpga_get_time_stamp(void)
+{
+	u64 cntpct;
+
+	isb();
+	asm volatile("mrs %0, cntpct_el0" : "=r" (cntpct));
+	return cntpct;
+}
+
+static __always_inline u64 __socfpga_usec_to_tick(u64 usec)
+{
+	u64 tick = usec;
+	u64 cntfrq;
+
+	asm volatile("mrs %0, cntfrq_el0" : "=r" (cntfrq));
+	tick *= cntfrq;
+	do_div(tick, 1000000);
+	return tick;
+}
+
+static __always_inline void __socfpga_udelay(u64 usec)
+{
+	u64 tmp = __socfpga_get_time_stamp() + __socfpga_usec_to_tick(usec);
+
+	while (__socfpga_get_time_stamp() < tmp + 1)
+		;
+}
 
 #endif
