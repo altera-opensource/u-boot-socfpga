9beb0c66133081855fa4cfb470cdc67123acb817
Richard Gong <richard.gong@intel.com>
2020-11-03 11:05:02 -0600

    HSD #22011642423: rsu: remove the default rsu_spt_checksum settings
    The environment variable rsu_spt_checksum is not set by default during
    system boots up. Therefore the settings made by the user can be
    overwritten.
    
    Remove the default rsu_spt_checksum settings to use whatever the user
    chooses.
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>

--- arch/arm/mach-socfpga/misc_soc64.c
+++ arch/arm/mach-socfpga/misc_soc64.c
@@ -77,9 +77,6 @@ int arch_misc_init(void)
 	env_set("rsu_protected_slot", "");
 	env_set("rsu_log_level", level);
 
-	/* setup for RSU sub-partition table checksum */
-	env_set("rsu_spt_checksum", "");
-
 	return 0;
 }
 #endif
