2e7b27cb732092474e47d22c87e8e5329b70c73e
Richard Gong <richard.gong@intel.com>
2020-11-03 12:21:49 -0600

    HSD #14012886181: rsu: remove the default rsu_protected_slot settings
    Remove the default rsu_protected_slot settings to use whatever the user
    choose.
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>

--- arch/arm/mach-socfpga/misc_soc64.c
+++ arch/arm/mach-socfpga/misc_soc64.c
@@ -114,7 +114,6 @@ int arch_misc_init(void)
 	env_set("qspi_clock", qspi_string);
 
 	/* setup for RSU */
-	env_set("rsu_protected_slot", "");
 	env_set("rsu_log_level", level);
 
 	/* Export board_id as environment variable */
