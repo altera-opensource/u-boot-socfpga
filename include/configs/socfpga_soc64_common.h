395e53deb23bdf2b6c211a3674fc2c5fde61197a
Radu Bacrau <radu.bacrau@intel.com>
2020-12-14 16:51:51 -0600

    HSD #14013046644: group RSU commands and query RSU max_retry from bootcmd
    Gathered all RSU commands from bootcmd on all supported configurations into a
    separate script called rsu_status.
    
    Added call to 'rsu display_max_retry' to all the supported configurations.
    Modified the error message displayed by the command on non-RSU systems to be
    consistent with existing error messages for the other RSU commands.
    
    Signed-off-by: Radu Bacrau <radu.bacrau@intel.com>
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- include/configs/socfpga_soc64_common.h
+++ include/configs/socfpga_soc64_common.h
@@ -221,6 +221,8 @@
 			" root=${nandroot} rw rootwait rootfstype=jffs2; " \
 			"booti ${loadaddr} - ${fdt_addr}\0" \
 	"socfpga_legacy_reset_compat=1\0" \
+	"rsu_status=rsu dtb; rsu display_dcmf_version; "\
+		"rsu display_dcmf_status; rsu display_max_retry\0" \
 	"smc_fid_rd=0xC2000007\0" \
 	"smc_fid_wr=0xC2000008\0" \
 	"smc_fid_upd=0xC2000009\0 "
