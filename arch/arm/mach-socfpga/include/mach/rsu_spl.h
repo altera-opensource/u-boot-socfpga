a70988d762d47c4426b872b439a3a8f63a962f8e
Radu Bacrau <radu.bacrau@intel.com>
2021-03-10 21:32:08 -0600

    arch: arm: rsu: Multiboot selection Uboot SSBL for RSU
    Use different Uboot in QSPI for each RSU bitstream based on the SPL.
    Selection is based on the naming of SSBL partition, e.g: P1 SPL will select
    SSBL.P1 for loading the Uboot SSBL.
    
    Signed-off-by: Kah Jing Lee <kah.jing.lee@intel.com>
    Signed-off-by: Radu Bacrau <radu.bacrau@intel.com>

---
+++ arch/arm/mach-socfpga/include/mach/rsu_spl.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier:    GPL-2.0 */
+/*
+ * Copyright (C) 2022 Intel Corporation <www.intel.com>
+ *
+ */
+
+#ifndef _RSU_S10_SPL_H_
+#define _RSU_S10_SPL_H_
+#include <asm/arch/rsu_s10.h>
+
+u32 rsu_spl_ssbl_address(void);
+u32 rsu_spl_ssbl_size(void);
+
+#endif /* _RSU_S10_SPL__H_ */
