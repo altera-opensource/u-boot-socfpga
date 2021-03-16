77672ded4da6970b6c9bf72622432ae14c885785
Tien Fong Chee <tien.fong.chee@intel.com>
2021-03-15 21:39:48 -0700

    arm: socfpga: Update system manager for n5x
    Both SYSMGR_SOC64_HMC_CLK AND SYSMGR_SOC64_IO_PA_CTRL registers are
    replaced by SYSMGR_SOC64_DDR_MODE in n5x. This patch updates the changes
    in system manager for n5x.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>
    
    ---
    
    v1->v2
    - Using #if IS_ENABLED(CONFIG ...) instead of #ifdef

--- drivers/ddr/altera/sdram_soc64.c
+++ drivers/ddr/altera/sdram_soc64.c
@@ -99,12 +99,14 @@ int emif_reset(struct altera_sdram_platdata *plat)
 	return 0;
 }
 
+#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_N5X)
 int poll_hmc_clock_status(void)
 {
 	return wait_for_bit_le32((const void *)(socfpga_get_sysmgr_addr() +
 				 SYSMGR_SOC64_HMC_CLK),
 				 SYSMGR_HMC_CLK_STATUS_MSK, true, 1000, false);
 }
+#endif
 
 void sdram_clear_mem(phys_addr_t addr, phys_size_t size)
 {
