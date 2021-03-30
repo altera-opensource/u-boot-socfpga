abef4cc3ab89765a79f3c8ffa0208724d7484664
Siew Chin Lim <elly.siew.chin.lim@intel.com>
2021-03-30 22:39:27 +0800

    drivers: watchdog: Enhance watchdog support in SPL for N5X
    Enable watchdog before initializing other component (example: DDR).
    Thus, watchdog need to be fully executed in onchip ram.
    
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- arch/arm/mach-socfpga/spl_n5x.c
+++ arch/arm/mach-socfpga/spl_n5x.c
@@ -19,7 +19,7 @@
 #include <asm/arch/reset_manager.h>
 #include <asm/arch/smmu_s10.h>
 #include <asm/arch/system_manager.h>
-#include <watchdog.h>
+#include <wdt.h>
 #include <dm/uclass.h>
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -38,13 +38,6 @@ void board_init_f(ulong dummy)
 	/* Ensure watchdog is paused when debugging is happening */
 	writel(SYSMGR_WDDBG_PAUSE_ALL_CPU,
 	       socfpga_get_sysmgr_addr() + SYSMGR_SOC64_WDDBG);
-
-#ifdef CONFIG_HW_WATCHDOG
-	/* Enable watchdog before initializing the HW */
-	socfpga_per_reset(SOCFPGA_RESET(L4WD0), 1);
-	socfpga_per_reset(SOCFPGA_RESET(L4WD0), 0);
-	hw_watchdog_init();
-#endif
 
 	/* ensure all processors are not released prior Linux boot */
 	writeq(0, CPU_RELEASE_ADDR);
@@ -66,6 +59,14 @@ void board_init_f(ulong dummy)
 		printf("Memory clock init failed: %d\n", ret);
 		hang();
 	}
+
+	/*
+	 * Enable watchdog as early as possible before initializing other
+	 * component. Watchdog need to be enabled after clock driver because
+	 * it will retrieve the clock frequency from clock driver.
+	 */
+	if (CONFIG_IS_ENABLED(WDT))
+		initr_watchdog();
 
 	print_reset_info();
 	cm_print_clock_quick_summary();
