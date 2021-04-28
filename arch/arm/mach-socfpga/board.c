c741bdbb703b2813e04d5c209f80759b1babd5db
Sin Hui Kho <sin.hui.kho@intel.com>
2021-04-28 16:12:42 +0800

    arm: socfpga: Move rsu_status from bootcommand to board_prep_linux function
    Move 'rsu_status' from bootcommand to board_prep_linux function when
    OS booted from FIT image for Stratix 10 and Agilex. This flow is common for
    all Intel SOC64 device.
    
    In 'rsu_status', it contains 'rsu dtb' command that will update Linux DTB
    qspi-boot partition offset with spt0 value before passing to Linux. Also,
    this is done after linux_qspi_enable.

--- arch/arm/mach-socfpga/board.c
+++ arch/arm/mach-socfpga/board.c
@@ -179,6 +179,8 @@ void board_prep_linux(struct bootm_headers *images)
 	if (use_fit && IS_ENABLED(CONFIG_CADENCE_QSPI)) {
 		if (env_get("linux_qspi_enable"))
 			run_command(env_get("linux_qspi_enable"), 0);
+		if (env_get("rsu_status"))
+			run_command(env_get("rsu_status"), 0);
 	}
 }
 #endif
