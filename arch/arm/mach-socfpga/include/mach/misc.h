b71fdaabed2c370f1a4142319b2a8489bfa90964
Tien Fong Chee <tien.fong.chee@intel.com>
2021-05-19 07:27:08 -0700

    ddr: altera: n5x: Add self-refresh support in DDR4
    Enable self-refresh support in DDR4 for retaining DDR4 content with minimal
    power when reset is triggered.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

--- arch/arm/mach-socfpga/include/mach/misc.h
+++ arch/arm/mach-socfpga/include/mach/misc.h
@@ -48,10 +48,6 @@ int qspi_flash_software_reset(void);
 int is_fpga_config_ready(void);
 #endif
 
-#if IS_ENABLED(CONFIG_TARGET_SOCFPGA_N5X)
-bool is_ddr_init_skipped(void);
-#endif
-
 void do_bridge_reset(int enable, unsigned int mask);
 void socfpga_pl310_clear(void);
 void socfpga_get_managers_addr(void);
