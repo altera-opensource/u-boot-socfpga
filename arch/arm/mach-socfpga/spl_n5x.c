6023067e4087d075dfad945e0574c8165d702c0d
Tien Fong Chee <tien.fong.chee@intel.com>
2021-06-30 02:59:32 -0700

    HSD #22011135953-4: arm: socfpga: Switch FW settings from codes to DTS
    Switching the firewall, and high privilege registers setting in source
    codes over to device tree implementation.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

--- arch/arm/mach-socfpga/spl_n5x.c
+++ arch/arm/mach-socfpga/spl_n5x.c
@@ -73,7 +73,11 @@ void board_init_f(ulong dummy)
 	print_reset_info();
 	cm_print_clock_quick_summary();
 
-	firewall_setup();
+	ret = uclass_get_device_by_name(UCLASS_NOP, "socfpga-secreg", &dev);
+	if (ret) {
+		printf("Firewall & secure settings init failed: %d\n", ret);
+		hang();
+	}
 
 	ret = uclass_get_device(UCLASS_CACHE, 0, &dev);
 	if (ret) {
