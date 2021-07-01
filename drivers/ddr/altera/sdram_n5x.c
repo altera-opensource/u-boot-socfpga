493195bfe819c36d5cb5d9aab1a37f5411e6ec44
Tien Fong Chee <tien.fong.chee@intel.com>
2021-07-01 01:33:34 -0700

    ddr: altera: n5x: Fixing debug log typo
    Fixing debug log typo.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

--- drivers/ddr/altera/sdram_n5x.c
+++ drivers/ddr/altera/sdram_n5x.c
@@ -2346,7 +2346,7 @@ static void set_cal_res_to_rankctrl(u32 reg_addr, u16 update_value,
 
 	debug("max value divided by 2 is 0x%x\n", update_value);
 	debug("umclt2 register 0x%x value is 0%x before ", reg_addr, reg);
-	debug("update with train result\n");
+	debug("updating with train result\n");
 
 	value = (reg & mask) >> shift;
 
@@ -2366,8 +2366,8 @@ static void set_cal_res_to_rankctrl(u32 reg_addr, u16 update_value,
 	writel((reg & (~mask)) | value, (uintptr_t)reg_addr);
 
 	reg = readl((uintptr_t)reg_addr);
-	debug("umclt2 register 0x%x value is 0%x before ", reg_addr, reg);
-	debug("update with train result\n");
+	debug("umclt2 register 0x%x value is 0%x after ", reg_addr, reg);
+	debug("updating with train result\n");
 }
 
 /* helper function for updating train result to register */
@@ -2380,7 +2380,7 @@ static void set_cal_res_to_reg(u32 reg_addr, u16 update_value, u32 mask,
 
 	debug("max value divided by 2 is 0x%x\n", update_value);
 	debug("umclt2 register 0x%x value is 0%x before ", reg_addr, reg);
-	debug("update with train result\n");
+	debug("updating with train result\n");
 
 	value = (reg & mask) >> shift;
 
@@ -2390,8 +2390,8 @@ static void set_cal_res_to_reg(u32 reg_addr, u16 update_value, u32 mask,
 	writel((reg & (~mask)) | value, (uintptr_t)reg_addr);
 
 	reg = readl((uintptr_t)reg_addr);
-	debug("umclt2 register 0x%x value is 0%x before ", reg_addr, reg);
-	debug("update with train result\n");
+	debug("umclt2 register 0x%x value is 0%x after ", reg_addr, reg);
+	debug("updating with train result\n");
 }
 
 static u16 get_max_txdqsdlytg0_ux_p0(struct ddr_handoff *handoff, u32 reg,
