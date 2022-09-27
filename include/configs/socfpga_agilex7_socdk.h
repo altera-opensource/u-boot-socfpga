963053c7a074b80ed3173c3426a46b2ff81c0360
Sin Hui Kho <sin.hui.kho@intel.com>
2022-09-28 00:02:44 +0800

    arm: socfpga: agilex7: Enable Intel Agilex7 SoC FPGA build
    Add build support for Intel Agilex7 SoC FPGA.
    
    Signed-off-by: Sin Hui Kho <sin.hui.kho@intel.com>

---
+++ include/configs/socfpga_agilex7_socdk.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2023 Intel Corporation <www.intel.com>
+ *
+ */
+
+#ifndef __CONFIG_SOCFGPA_AGILEX7_H__
+#define __CONFIG_SOCFGPA_AGILEX7_H__
+
+#include <configs/socfpga_soc64_common.h>
+
+#endif	/* __CONFIG_SOCFGPA_AGILEX7_H__ */
