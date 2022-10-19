1b205b2117bfaa3edd91c01de9cd91f8195932be
Teik Heng Chong <teik.heng.chong@intel.com>
2022-10-19 10:45:51 +0800

    HSD #15011972255: drivers: mtd: spi: Add support for GD55LB02GEBIR SPI NOR flash
    Add Support for GigaDevice GD55LB02GEBIR SPI NOR flash as QSPI
    configuration flash
    
    Signed-off-by: Teik Heng Chong <teik.heng.chong@intel.com>

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -127,6 +127,11 @@ const struct flash_info spi_nor_ids[] = {
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
+	{
+		INFO("gd55lb02ge", 0xc8671c, 0, 64 * 1024, 4096,
+		     SECT_4K | SPI_NOR_QUAD_READ |
+		     SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
+	},
 #endif
 #ifdef CONFIG_SPI_FLASH_ISSI		/* ISSI */
 	/* ISSI */
