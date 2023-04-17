973e0dd597a9a2347c87ddf0f659ce511ccfa9db
Dinesh Maniyam <dinesh.maniyam@intel.com>
2023-04-17 15:48:29 +0800

    agilex5: revert and clean previous commits
    Signed-off-by: Dinesh Maniyam <dinesh.maniyam@intel.com>

--- arch/arm/mach-socfpga/misc.c
+++ arch/arm/mach-socfpga/misc.c
@@ -254,18 +254,18 @@ void socfpga_get_managers_addr(void)
 	if (ret)
 		hang();
 
-	if IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX)
-		ret = socfpga_get_base_addr("intel,agilex-clkmgr",
-					    &socfpga_clkmgr_base);
-	else if IS_ENABLED(CONFIG_TARGET_SOCFPGA_N5X)
-		ret = socfpga_get_base_addr("intel,n5x-clkmgr",
-					    &socfpga_clkmgr_base);
-	else if IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5)
-		ret = socfpga_get_base_addr("intel,agilex5-clkmgr",
-					    &socfpga_clkmgr_base);
-	else
-		ret = socfpga_get_base_addr("altr,clk-mgr", &socfpga_clkmgr_base);
-
+#ifdef CONFIG_TARGET_SOCFPGA_AGILEX
+	ret = socfpga_get_base_addr("intel,agilex-clkmgr",
+				    &socfpga_clkmgr_base);
+#elif IS_ENABLED(CONFIG_TARGET_SOCFPGA_N5X)
+	ret = socfpga_get_base_addr("intel,n5x-clkmgr",
+				    &socfpga_clkmgr_base);
+#elif IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5)
+	ret = socfpga_get_base_addr("intel,agilex5-clkmgr",
+				    &socfpga_clkmgr_base);
+#else
+	ret = socfpga_get_base_addr("altr,clk-mgr", &socfpga_clkmgr_base);
+#endif
 	if (ret)
 		hang();
 }
