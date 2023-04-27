2ba8797cb1ddc958f23f63f270d497b216c07b31
Jit Loon Lim <jit.loon.lim@intel.com>
2023-04-27 22:15:58 +0800

    rst-mgr-agx5: agilex5: Implement NAND and DMA deassert
    This patch is to implement deassert of NAND and DMA for Agilex5.
    This patch shall be reverted once the driver for NAND/DMA is ready.
    The deassert shall be done in the driver.
    
    Signed-off-by: Jit Loon Lim <jit.loon.lim@intel.com>

--- drivers/reset/reset-socfpga.c
+++ drivers/reset/reset-socfpga.c
@@ -24,9 +24,16 @@
 #include <linux/bitops.h>
 #include <linux/io.h>
 #include <linux/sizes.h>
+#include <asm/arch/reset_manager.h>
 
-#define BANK_INCREMENT		4
-#define NR_BANKS		8
+#define BANK_INCREMENT				4
+#define NR_BANKS				8
+#define RSTMGR_PER0MODRST_NAND			BIT(5)
+#define RSTMGR_PER0MODRST_DMA			BIT(16)
+#define RSTMGR_PER0MODRST_DMAIF			GENMASK(31,24)
+#define RSTMGR_PER0MODRST_NAND_DMA_DEASSERT	RSTMGR_PER0MODRST_NAND \
+						| RSTMGR_PER0MODRST_DMA \
+						| RSTMGR_PER0MODRST_DMAIF
 
 struct socfpga_reset_data {
 	void __iomem *modrst_base;
@@ -111,6 +118,16 @@ static int socfpga_reset_probe(struct udevice *dev)
 static int socfpga_reset_remove(struct udevice *dev)
 {
 	struct socfpga_reset_data *data = dev_get_priv(dev);
+
+/*
+ * TODO: This is temporary solution for NAND/DMA deaasert.
+ * When NAND/DMA driver is ready, the deassert shall be done
+ * from NAND/DMA driver.
+ */
+#if defined(CONFIG_TARGET_SOCFPGA_AGILEX5)
+	clrbits_le32(socfpga_get_rstmgr_addr() + RSTMGR_SOC64_PER0MODRST, \
+		     RSTMGR_PER0MODRST_NAND_DMA_DEASSERT);
+#endif
 
 	if (socfpga_reset_keep_enabled()) {
 		puts("Deasserting all peripheral resets\n");
