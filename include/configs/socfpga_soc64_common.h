60b36e387ec04c9fdc0df1450aeef5bc60a23c4d
Dinesh Maniyam <dinesh.maniyam@intel.com>
2023-11-17 15:49:53 +0800

    HSD #16022125041: configs: Update env to support NAND and QSPI autoboot
    This patch is to enhance env to support NAND and QSPI autoboot in socfpga
    devices. UBIFS can only read env file from single distinct root partition
    from boot media. Thus this env needed to avoid QSPI to call nand root
    partition and NAND to call qspi root partition and causing script failure.
    
    Signed-off-by: Dinesh Maniyam <dinesh.maniyam@intel.com>

--- include/configs/socfpga_soc64_common.h
+++ include/configs/socfpga_soc64_common.h
@@ -98,6 +98,10 @@
 
 #define BOOTENV_DEV_QSPI(devtypeu, devtypel, instance) \
 	"bootcmd_qspi=ubi detach; sf probe && " \
+	"setenv mtdids 'nor0=nor0,nand0=nand.0' && " \
+	"setenv mtdparts 'mtdparts=nor0:66m(u-boot),190m(root);nand.0:2m(u-boot),500m(nand_root)' && " \
+	"env select UBI; saveenv && " \
+	"ubi part root && " \
 	"if ubi part root && ubi readvol ${scriptaddr} script; " \
 	"then echo QSPI: Running script from UBIFS; " \
 	"elif sf read ${scriptaddr} ${qspiscriptaddr} ${scriptsize}; " \
@@ -116,7 +120,11 @@
 #endif
 
 #define BOOTENV_DEV_NAND(devtypeu, devtypel, instance) \
-	"bootcmd_nand=ubi detach; ubi part root && " \
+	"bootcmd_nand=ubi detach && " \
+	"setenv mtdids 'nor0=nor0,nand0=nand.0' && " \
+	"setenv mtdparts 'mtdparts=nor0:66m(u-boot),190m(qspi_root);nand.0:2m(u-boot),500m(root)' && " \
+	"env select UBI; saveenv && " \
+	"ubi part root && " \
 	"ubi readvol ${scriptaddr} script && " \
 	"echo NAND: Trying to boot script at ${scriptaddr} && " \
 	"source ${scriptaddr}; " \
