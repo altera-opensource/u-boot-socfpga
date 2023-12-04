cce0b1c522f6b3abe8fa675654207fe7cb4c847f
Tien Fong Chee <tien.fong.chee@intel.com>
2023-12-04 18:21:16 +0800

    Revert "arch: arm: mach-socfpga: misc_soc64: Disable SMMU"
    This reverts commit d1af474c101a6e17c8bec71827853dc33a91e7c8.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@intel.com>

--- arch/arm/mach-socfpga/misc_soc64.c
+++ arch/arm/mach-socfpga/misc_soc64.c
@@ -143,13 +143,10 @@ void arch_preboot_os(void)
 
 int misc_init_r(void)
 {
-/* TODO: Fix SMMU w/ DCACHE Enabled */
-/*
 #if IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5)
 	if (is_agilex5_reva_workaround_required())
 		return smmu_sdm_init();
 #endif
-*/
 
 	return 0;
 }
