51ca2e0c09d3bff92f907251c39f786fc4aaf767
Dinesh Maniyam <dinesh.maniyam@intel.com>
2024-03-13 03:40:38 +0800

    HSD #15015141122-9: drivers: mtd: nand: Force disable the ECC for A0
    This patch is to disable ECC for A0 silicon as it has an issue
    with SPRAM that causing last few bytes of data incorrect.
    
    Signed-off-by: Dinesh Maniyam <dinesh.maniyam@intel.com>

--- arch/arm/mach-socfpga/misc_soc64.c
+++ arch/arm/mach-socfpga/misc_soc64.c
@@ -93,7 +93,7 @@ int arch_early_init_r(void)
 }
 
 #if IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5)
-static bool is_agilex5_reva_workaround_required(void)
+bool is_agilex5_reva_workaround_required(void)
 {
 	u32 reg;
 	bool status;
