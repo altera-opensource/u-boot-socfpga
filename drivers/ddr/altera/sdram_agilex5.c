8d24a894be9e9b125add9a631d5ce99969d38757
Teik Heng Chong <teik.heng.chong@intel.com>
2024-03-18 11:48:15 +0800

    HSD #15014833245-2: ddr: altera: iossm: Group multiple IO96B instance into an array
    This patch is to group multiple IO96B instance into an array
    
    Signed-off-by: Teik Heng Chong <teik.heng.chong@intel.com>

--- drivers/ddr/altera/sdram_agilex5.c
+++ drivers/ddr/altera/sdram_agilex5.c
@@ -118,20 +118,9 @@ int populate_ddr_handoff(struct udevice *dev, struct io96b_info *io96b_ctrl)
 		if (addr == FDT_ADDR_T_NONE)
 			return -EINVAL;
 
-		switch (i) {
-		case 0:
-			io96b_ctrl->io96b_0.io96b_csr_addr = addr;
-			debug("%s: IO96B 0x%llx CSR enabled\n", __func__
-					, io96b_ctrl->io96b_0.io96b_csr_addr);
-			break;
-		case 1:
-			io96b_ctrl->io96b_1.io96b_csr_addr = addr;
-			debug("%s: IO96B 0x%llx CSR enabled\n", __func__
-					, io96b_ctrl->io96b_1.io96b_csr_addr);
-			break;
-		default:
-			printf("%s: Invalid IO96B CSR\n", __func__);
-		}
+		io96b_ctrl->io96b[i].io96b_csr_addr = addr;
+		debug("%s: IO96B 0x%llx CSR enabled\n", __func__
+			, io96b_ctrl->io96b[i].io96b_csr_addr);
 	}
 
 	return 0;
@@ -182,6 +171,7 @@ bool ddr_ecc_dbe_status(void)
 int sdram_mmr_init_full(struct udevice *dev)
 {
 	int ret;
+	int i;
 	phys_size_t hw_size;
 	struct bd_info bd = {0};
 	struct altera_sdram_plat *plat = dev_get_plat(dev);
@@ -228,10 +218,10 @@ int sdram_mmr_init_full(struct udevice *dev)
 
 	/* Need to trigger re-calibration for DDR DBE */
 	if (ddr_ecc_dbe_status()) {
-		io96b_ctrl->io96b_0.cal_status = false;
-		io96b_ctrl->io96b_1.cal_status = false;
-		io96b_ctrl->overall_cal_status = io96b_ctrl->io96b_0.cal_status ||
-						 io96b_ctrl->io96b_1.cal_status;
+		for (i = 0; i < io96b_ctrl->num_instance; i++)
+			io96b_ctrl->io96b[i].cal_status = false;
+
+		io96b_ctrl->overall_cal_status = false;
 	}
 
 	/* Trigger re-calibration if calibration failed */
