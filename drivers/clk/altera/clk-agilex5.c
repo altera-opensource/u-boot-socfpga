9a6b031375a65dc2b4efb51ea97695a17be267b2
Tingting Meng <tingting.meng@intel.com>
2024-08-14 23:29:03 +0800

    HSD #15016500042: drivers: clk: agilex5: Configure intosc as boot_clk source
    This patch enables the configuration of the HPS internal oscillator as
    the boot_clk source instead of the default external
    oscillator(HPS_OSC_CLK) in non-secure boot scenarios. Some customers
    prefer to minimize the use of external oscillators, especially when using
    the FPGA first configuration mode. This change allows them to eliminate
    the need for an additional oscillator device and a dedicated HPS pin,
    simplifying board layout and routing.
    
    Signed-off-by: Tingting Meng <tingting.meng@intel.com>

--- drivers/clk/altera/clk-agilex5.c
+++ drivers/clk/altera/clk-agilex5.c
@@ -26,6 +26,9 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+#define CLKMGR_CTRL_SWCTRLBTCLKEN_MASK		BIT(8)
+#define CLKMGR_CTRL_SWCTRLBTCLKSEL_MASK		BIT(9)
+
 struct socfpga_clk_plat {
 	void __iomem *regs;
 };
@@ -264,9 +267,14 @@ static void clk_basic_init(struct udevice *dev,
 			       CM_REG_READL(plat, CLKMGR_CTRL) & ~CLKMGR_CTRL_BOOTMODE);
 	} else {
 #ifdef CONFIG_XPL_BUILD
-		/* Always force clock manager into boot mode before any configuration */
-		clk_write_ctrl(plat,
-			       CM_REG_READL(plat, CLKMGR_CTRL) | CLKMGR_CTRL_BOOTMODE);
+		/*
+		 * Configure HPS Internal Oscillator as default boot_clk source,
+		 * always force clock manager into boot mode before any configuration
+		 */
+		clk_write_ctrl(plat, CM_REG_READL(plat, CLKMGR_CTRL) |
+			       CLKMGR_CTRL_BOOTMODE |
+			       CLKMGR_CTRL_SWCTRLBTCLKEN_MASK |
+			       CLKMGR_CTRL_SWCTRLBTCLKSEL_MASK);
 #else
 		/* Skip clock configuration in SSBL if it's not in boot mode */
 		if (!(CM_REG_READL(plat, CLKMGR_CTRL) & CLKMGR_CTRL_BOOTMODE))
