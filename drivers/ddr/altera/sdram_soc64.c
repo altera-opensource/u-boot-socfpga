350b2e9c1a61069ccde7114407c481000910ea3a
Tingting Meng <tingting.meng@altera.com>
2025-05-02 12:35:10 +0800

    HSD #14021947418: ddr: altera: agilex9: Remove calibration completion check
    The bit[3] of the seq2core register is no longer set to indicate
    calibration completion. Instead, we now rely on polling the calibration
    status directly to ensure it passes. The check for the bit[3] is removed
    accordingly.
    
    Signed-off-by: Tingting Meng <tingting.meng@altera.com>

--- drivers/ddr/altera/sdram_soc64.c
+++ drivers/ddr/altera/sdram_soc64.c
@@ -85,16 +85,6 @@ int emif_reset(struct altera_sdram_plat *plat)
 	debug("DDR: Triggerring emif reset\n");
 	hmc_ecc_writel(plat, DDR_HMC_CORE2SEQ_INT_REQ, RSTHANDSHAKECTRL);
 
-	/* if seq2core[3] = 0, we are good */
-	ret = wait_for_bit_le32((const void *)(plat->hmc +
-				 RSTHANDSHAKESTAT),
-				 DDR_HMC_SEQ2CORE_INT_RESP_MASK,
-				 false, 1000, false);
-	if (ret) {
-		printf("DDR: failed to get ack from EMIF\n");
-		return ret;
-	}
-
 	ret = emif_clear(plat);
 	if (ret) {
 		printf("DDR: emif_clear() failed\n");
