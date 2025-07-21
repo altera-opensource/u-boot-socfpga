31acb41c288eb5ce48b07faa773a97d7820fd7bc
Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>
2025-07-21 09:36:19 +0800

    arm: socfpga: misc: Probe sysmgr base address for legacy devices
    Agilex5 already probed the system manager driver for the base address
    during SPL initialization, legacy devices should follow this
    implementation as well.
    
    Keep the downstream implementation for now as we work on upstreaming
    all the legacy devices' updates.
    
    Signed-off-by: Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>

--- arch/arm/mach-socfpga/misc.c
+++ arch/arm/mach-socfpga/misc.c
@@ -270,6 +270,15 @@ void socfpga_get_managers_addr(void)
 	if (ret)
 		hang();
 
+	if (!IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX) &&
+	    !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX7M) &&
+	    !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5)) {
+		ret = socfpga_get_base_addr("altr,sys-mgr",
+					    &socfpga_sysmgr_base);
+		if (ret)
+			hang();
+	}
+
 	else if (IS_ENABLED(CONFIG_TARGET_SOCFPGA_N5X))
 		ret = socfpga_get_base_addr("intel,n5x-clkmgr",
 					    &socfpga_clkmgr_base);
