a7c1be8dceecd4f31d649952e6a03347ce0fae48
Tanmay Kathpalia <tanmay.kathpalia@altera.com>
2025-08-01 02:54:44 +0800

    HSD #16026967012: arch: socfpga: agilex5: Refactor MMC initialization in SPL
    Changes:
    - Replace CONFIG_IS_ENABLED(PHY_CADENCE_COMBOPHY) with
      IS_ENABLED(CONFIG_SPL_MMC)
    - Add MMC initialization including:
      * DFI_SEL configuration for SDMMC
      * Set secure transaction register for SDMMC
    
    Signed-off-by: Tanmay Kathpalia <tanmay.kathpalia@altera.com>

--- arch/arm/mach-socfpga/spl_agilex5.c
+++ arch/arm/mach-socfpga/spl_agilex5.c
@@ -12,6 +12,7 @@
 #include <spl.h>
 #include <asm/arch/base_addr_soc64.h>
 #include <asm/arch/clock_manager.h>
+#include <asm/arch/firewall.h>
 #include <asm/arch/mailbox_s10.h>
 #include <asm/arch/misc.h>
 #include <asm/arch/reset_manager.h>
@@ -114,6 +115,19 @@ void board_init_f(ulong dummy)
 			debug("DRAM init failed: %d\n", ret);
 			hang();
 		}
+	}
+
+	/*
+	 * Below condition code needs to be removed once DFI Select is moved to
+	 * the correct place.
+	 */
+	if (IS_ENABLED(CONFIG_SPL_MMC)) {
+		u32 tmp = SYSMGR_SOC64_COMBOPHY_DFISEL_SDMMC;
+		/* configure DFI_SEL for SDMMC */
+		writel(tmp, socfpga_get_sysmgr_addr() + SYSMGR_SOC64_COMBOPHY_DFISEL);
+
+		/* Dinesh to confirm for NAND, whether secure transaction are required. */
+		writel(SECURE_TRANS_SET, SECURE_TRANS_REG);
 	}
 
 	if (IS_ENABLED(CONFIG_CADENCE_QSPI))
