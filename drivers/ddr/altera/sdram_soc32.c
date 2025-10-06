460359b73f4138a651b4655e99c268a419b57668
Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>
2025-10-06 10:15:30 +0800

    HSD #15018450482-1: arm: socfpga: Consolidate dram_bank_mmu_setup()
    Relocate the dram_bank_mmu_setup() implementation from misc_arria10.c to
    the common socfpga misc.c and update the function to correctly handle both
    pre-relocation and post-relocation cases for DRAM cache enabling for
    consistent MMU/dcache setup across Arria10 and CycloneV platforms.
    
    These changes help to improve maintainability and consistency of DRAM
    initialization as well as MMU configuration for Arria10 and CycloneV
    platforms.
    
    Signed-off-by: Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>

--- drivers/ddr/altera/sdram_soc32.c
+++ drivers/ddr/altera/sdram_soc32.c
@@ -21,15 +21,8 @@ DECLARE_GLOBAL_DATA_PTR;
 void sdram_init_ecc_bits(void)
 {
 	u32 start;
-	phys_addr_t start_addr, saved_start;
-	phys_size_t size, size_init, saved_size;
-#if defined(CONFIG_SYS_ARM_CACHE_WRITETHROUGH)
-	enum dcache_option option = DCACHE_WRITETHROUGH;
-#elif defined(CONFIG_SYS_ARM_CACHE_WRITEALLOC)
-	enum dcache_option option = DCACHE_WRITEALLOC;
-#else
-	enum dcache_option option = DCACHE_WRITEBACK;
-#endif
+	phys_addr_t start_addr;
+	phys_size_t size, size_init;
 
 	start = get_timer(0);
 
@@ -42,14 +35,9 @@ void sdram_init_ecc_bits(void)
 	gd->arch.tlb_addr = start_addr + PGTABLE_OFF;
 	gd->arch.tlb_size = PGTABLE_SIZE;
 	start_addr += PGTABLE_SIZE + PGTABLE_OFF;
-	size -= (PGTABLE_OFF + PGTABLE_SIZE);
-	dcache_enable();
+	size -= PGTABLE_OFF + PGTABLE_SIZE;
 
-	saved_start = start_addr;
-	saved_size = size;
-	/* Set SDRAM region to writethrough to avoid false double-bit error. */
-	mmu_set_region_dcache_behaviour(saved_start, saved_size,
-					DCACHE_WRITETHROUGH);
+	dcache_enable();
 
 	while (size > 0) {
 		size_init = min((phys_addr_t)SZ_1G, (phys_addr_t)size);
@@ -79,10 +67,6 @@ void sdram_init_ecc_bits(void)
 	}
 
 	dcache_disable();
-
-	/* Restore back to original dcache behaviour. */
-	mmu_set_region_dcache_behaviour(saved_start, saved_size,
-					option);
 
 	printf("DDRCAL: SDRAM-ECC initialized success with %d ms\n",
 	       (u32)get_timer(start));
