57dc14ef9f2d11a0ecd8efcd4b058a025cce4854
Ng Tze Yee <tze.yee.ng@altera.com>
2026-01-29 14:51:01 +0800

    HSD #15018959703: Revert "HSD #16026967012: arm: mach-socfpga: Add eMMC HS400 data strobe pull-down configuration for Agilex5" (#789)
    HSD #15018959703: This reverts commit
    7f9c7242a93192bac67e0d7c78339120994e668a. It was a workaround for GHRD.
    Remove this workaround as the fix is implemented in GHRD.
    
    Regression Test result: 
    SM42 B0 eMMC -
    [emmc_hs400_sm42b0_newuboot.log](https://github.com/user-attachments/files/24903935/emmc_hs400_sm42b0_newuboot.log)
    SM72 B0 eMMC -
    [emmc_hs400_test_sm72b0_newuboot.log](https://github.com/user-attachments/files/24929469/emmc_hs400_test_sm72b0_newuboot.log)
    
    Signed-off-by: tzeyeeng <tze.yee.ng@intel.com>

--- arch/arm/mach-socfpga/spl_agilex5.c
+++ arch/arm/mach-socfpga/spl_agilex5.c
@@ -125,14 +125,6 @@ void board_init_f(ulong dummy)
 	if (IS_ENABLED(CONFIG_SPL_MMC))
 		writel(SECURE_TRANS_SET, SECURE_TRANS_REG);
 
-	if (IS_ENABLED(CONFIG_SPL_MMC_HS400_SUPPORT)) {
-		/* Below configuration for the data strobe pull down need to be removed
-		 * once eMMC GHRD is updated.
-		 */
-		printf("%s %d Set data strobe 20k ohm pull down\n", __func__, __LINE__);
-		writel(0x94, 0x10D1322C);
-	}
-
 	if (IS_ENABLED(CONFIG_CADENCE_QSPI))
 		mbox_qspi_open();
 
