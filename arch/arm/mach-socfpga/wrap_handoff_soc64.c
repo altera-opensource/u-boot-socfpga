390380e5260258e5ce445de65d12ccfd14ca7a49
Chen Huei Lok <chen.huei.lok@altera.com>
2026-01-29 11:04:45 +0800

    EUBU-652_3_3: arm: socfpga: soc64: Use dedicated IO base for pinmux and FPGA interface
    Update pinmux and FPGA interface register accesses to use
    SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS instead of the system manager base.
    
    This refactor improves clarity and isolates the dedicated pinmux
    register region from the general system manager base.
    
    No functional change intended.
    
    Signed-off-by: Chen Huei Lok <chen.huei.lok@altera.com>

--- arch/arm/mach-socfpga/wrap_handoff_soc64.c
+++ arch/arm/mach-socfpga/wrap_handoff_soc64.c
@@ -6,6 +6,7 @@
  */
 #include <errno.h>
 #include <inttypes.h>
+#include <asm/arch/base_addr_soc64.h>
 #include <asm/arch/handoff_soc64.h>
 #include <asm/arch/system_manager.h>
 #include <asm/io.h>
@@ -169,31 +170,31 @@ static void populate_sysmgr_fpgaintf_module(void)
 	u32 handoff_val = 0;
 
 	/* Enable the signal for those HPS peripherals that use FPGA. */
-	if (readl(socfpga_get_sysmgr_addr() + SYSMGR_SOC64_NAND_USEFPGA) ==
-	    SYSMGR_FPGAINTF_USEFPGA)
-		handoff_val |= SYSMGR_FPGAINTF_NAND;
-	if (readl(socfpga_get_sysmgr_addr() + SYSMGR_SOC64_SDMMC_USEFPGA) ==
-	    SYSMGR_FPGAINTF_USEFPGA)
-		handoff_val |= SYSMGR_FPGAINTF_SDMMC;
-	if (readl(socfpga_get_sysmgr_addr() + SYSMGR_SOC64_SPIM0_USEFPGA) ==
-	    SYSMGR_FPGAINTF_USEFPGA)
-		handoff_val |= SYSMGR_FPGAINTF_SPIM0;
-	if (readl(socfpga_get_sysmgr_addr() + SYSMGR_SOC64_SPIM1_USEFPGA) ==
-	    SYSMGR_FPGAINTF_USEFPGA)
-		handoff_val |= SYSMGR_FPGAINTF_SPIM1;
+	if (readl(SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS + SOC64_NAND_USEFPGA) ==
+	    HANDOFF_FPGAINTF_USEFPGA)
+		handoff_val |= HANDOFF_FPGAINTF_NAND;
+	if (readl(SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS + SOC64_SDMMC_USEFPGA) ==
+	    HANDOFF_FPGAINTF_USEFPGA)
+		handoff_val |= HANDOFF_FPGAINTF_SDMMC;
+	if (readl(SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS + SOC64_SPIM0_USEFPGA) ==
+	    HANDOFF_FPGAINTF_USEFPGA)
+		handoff_val |= HANDOFF_FPGAINTF_SPIM0;
+	if (readl(SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS + SOC64_SPIM1_USEFPGA) ==
+	    HANDOFF_FPGAINTF_USEFPGA)
+		handoff_val |= HANDOFF_FPGAINTF_SPIM1;
 	writel(handoff_val,
 	       socfpga_get_sysmgr_addr() + SYSMGR_SOC64_FPGAINTF_EN2);
 
 	handoff_val = 0;
-	if (readl(socfpga_get_sysmgr_addr() + SYSMGR_SOC64_EMAC0_USEFPGA) ==
-	    SYSMGR_FPGAINTF_USEFPGA)
-		handoff_val |= SYSMGR_FPGAINTF_EMAC0;
-	if (readl(socfpga_get_sysmgr_addr() + SYSMGR_SOC64_EMAC1_USEFPGA) ==
-	    SYSMGR_FPGAINTF_USEFPGA)
-		handoff_val |= SYSMGR_FPGAINTF_EMAC1;
-	if (readl(socfpga_get_sysmgr_addr() + SYSMGR_SOC64_EMAC2_USEFPGA) ==
-	    SYSMGR_FPGAINTF_USEFPGA)
-		handoff_val |= SYSMGR_FPGAINTF_EMAC2;
+	if (readl(SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS + SOC64_EMAC0_USEFPGA) ==
+	    HANDOFF_FPGAINTF_USEFPGA)
+		handoff_val |= HANDOFF_FPGAINTF_EMAC0;
+	if (readl(SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS + SOC64_EMAC1_USEFPGA) ==
+	    HANDOFF_FPGAINTF_USEFPGA)
+		handoff_val |= HANDOFF_FPGAINTF_EMAC1;
+	if (readl(SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS + SOC64_EMAC2_USEFPGA) ==
+	    HANDOFF_FPGAINTF_USEFPGA)
+		handoff_val |= HANDOFF_FPGAINTF_EMAC2;
 	writel(handoff_val,
 	       socfpga_get_sysmgr_addr() + SYSMGR_SOC64_FPGAINTF_EN3);
 }
@@ -221,8 +222,8 @@ static void populate_sysmgr_pinmux(void)
 	for (i = 0; i < len; i = i + 2) {
 		writel(handoff_table[i + 1],
 		       handoff_table[i] +
-		       (u8 *)socfpga_get_sysmgr_addr() +
-		       SYSMGR_SOC64_PINSEL0);
+		       (u8 *)SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS +
+		       SOC64_PINSEL0);
 	}
 
 	/* setup the pin ctrl */
@@ -231,8 +232,8 @@ static void populate_sysmgr_pinmux(void)
 	for (i = 0; i < len; i = i + 2) {
 		writel(handoff_table[i + 1],
 		       handoff_table[i] +
-		       (u8 *)socfpga_get_sysmgr_addr() +
-		       SYSMGR_SOC64_IOCTRL0);
+		       (u8 *)SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS +
+		       SOC64_IOCTRL0);
 	}
 
 	/* setup the fpga use */
@@ -255,8 +256,8 @@ static void populate_sysmgr_pinmux(void)
 #endif
 		writel(handoff_table[i + 1],
 		       handoff_table[i] +
-		       (u8 *)socfpga_get_sysmgr_addr() +
-		       SYSMGR_SOC64_EMAC0_USEFPGA);
+		       (u8 *)SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS +
+		       SOC64_EMAC0_USEFPGA);
 	}
 
 	/* setup the IO delay */
@@ -265,8 +266,8 @@ static void populate_sysmgr_pinmux(void)
 	for (i = 0; i < len; i = i + 2) {
 		writel(handoff_table[i + 1],
 		       handoff_table[i] +
-		       (u8 *)socfpga_get_sysmgr_addr() +
-		       SYSMGR_SOC64_IODELAY0);
+		       (u8 *)SOCFPGA_PINMUX_DEDICATED_IO_ADDRESS +
+		       SOC64_IODELAY0);
 	}
 }
 
