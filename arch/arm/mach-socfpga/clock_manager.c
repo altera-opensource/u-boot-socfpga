altera-opensource/u-boot-socfpga

commit c29e714f0395f0c99f633a670e553e2e844487c7

HSD #15018318288: arm: socfpga: agilex5: Add debug toolkit
U-Boot SPL debug toolkit makes every critical early boot
step visible and traceable:
- All handoff data is logged in detail (address, length, values) in U-Boot
  SPL.​
- PLL lock status and clock frequencies are logged as soon as possible.​
- Pre-console buffer: All logs before UART is up are captured and
  printed in order, so nothing is missed.​
- Memory test: After console-up, each address and pattern test is logged,
  making failures easy to pinpoint.​
- One config change enables debug output for all drivers and subsystems,
  no code patching required.​

Signed-off-by: Tien Fong Chee <tien.fong.chee@altera.com>

arch/arm/mach-socfpga/clock_manager.c
@@ -3,6 +3,7 @@
  *  Copyright (C) 2013-2017 Altera Corporation <www.altera.com>
  */
 
+#include <log.h>
 #include <asm/arch/clock_manager.h>
 #include <asm/arch/system_manager.h>
 #include <asm/global_data.h>
@@ -17,6 +18,10 @@ void cm_wait_for_lock(u32 mask)
 {
 	u32 inter_val;
 	u32 retry = 0;
+
+	log_info("%s : line %d => Is clock PLL locked? Checking ...\n",
+		 __FILE__, __LINE__);
+
 	do {
 #if defined(CONFIG_TARGET_SOCFPGA_GEN5)
 		inter_val = readl(socfpga_get_clkmgr_addr() +
@@ -33,6 +38,9 @@ void cm_wait_for_lock(u32 mask)
 		if (retry >= 10)
 			break;
 	} while (1);
+
+	log_info("%s : line %d => Clock PLL is locked !\n",
+		 __FILE__, __LINE__);
 }
 
 /* function to poll in the fsm busy bit */

arch/arm/mach-socfpga/wrap_handoff_soc64.c
@@ -4,11 +4,11 @@
  * Copyright (C) 2025 Altera Corporation <www.altera.com>
  *
  */
-
 #include <errno.h>
+#include <inttypes.h>
+#include <log.h>
 #include <asm/arch/handoff_soc64.h>
 #include <asm/io.h>
-#include "log.h"
 
 #ifndef __ASSEMBLY__
 #include <asm/types.h>
@@ -36,24 +36,19 @@ static enum endianness check_endianness(u32 handoff)
 		return BIG_ENDIAN;
 	case SOC64_HANDOFF_MAGIC_CLOCK:
 #if IS_ENABLED(CONFIG_TARGET_SOCFPGA_N5X)
-		debug("%s: mem clk  handoff data\n", __func__);
 		return LITTLE_ENDIAN;
 #else
 		return BIG_ENDIAN;
 #endif
 #if IS_ENABLED(CONFIG_TARGET_SOCFPGA_N5X)
 	case SOC64_HANDOFF_DDR_UMCTL2_MAGIC:
-		debug("%s: umctl2 handoff data\n", __func__);
 		return LITTLE_ENDIAN;
 	case SOC64_HANDOFF_DDR_PHY_MAGIC:
-		debug("%s: PHY handoff data\n", __func__);
 		return LITTLE_ENDIAN;
 	case SOC64_HANDOFF_DDR_PHY_INIT_ENGINE_MAGIC:
-		debug("%s: PHY engine handoff data\n", __func__);
 		return LITTLE_ENDIAN;
 #endif
 	default:
-		debug("%s: Unknown endianness!!\n", __func__);
 		return UNKNOWN_ENDIANNESS;
 	}
 }
@@ -67,8 +62,9 @@ static int getting_endianness(void *handoff_address, enum endianness *endian_t)
 		/* Trying to check handoff data is big endian? */
 		*endian_t = check_endianness(swab32(readl(handoff_address)));
 		if (*endian_t == UNKNOWN_ENDIANNESS) {
-			debug("%s: Cannot find HANDOFF MAGIC ", __func__);
-			debug("at addr 0x%p\n", (u32 *)handoff_address);
+			log_info("%s : line %d => Cannot find endianness ", __FILE__,
+				 __LINE__);
+			log_info("at addr 0x%p\n", (u32 *)handoff_address);
 			return -EPERM;
 		}
 	}
@@ -92,8 +88,8 @@ int socfpga_get_handoff_size(void *handoff_address)
 
 	size = (size - SOC64_HANDOFF_OFFSET_DATA) / sizeof(u32);
 
-	debug("%s: handoff address = 0x%p handoff size = 0x%08x\n", __func__,
-	      (u32 *)handoff_address, size);
+	log_info("%s : line %d => handoff address = 0x%p handoff size = 0x%08x\n", __FILE__,
+		 __LINE__, (u32 *)handoff_address, size);
 
 	return size;
 }
@@ -110,21 +106,27 @@ int socfpga_handoff_read(void *handoff_address, void *table, u32 table_len)
 	if (ret)
 		return ret;
 
+	log_info("%s : line %d => Handoff table address = 0x%p, ", __FILE__,
+		 __LINE__, table_x32);
+	log_info("Table length = 0x%x\n\n", table_len);
+
+	temp = readl(handoff_address);
+	log_info("%s : line %d => Handoff section : [%c%c%c%c] at 0x%08lx\n\n",
+		 __FILE__, __LINE__, (temp >> 0) & 0xff, (temp >> 8) & 0xff,
+		(temp >> 16) & 0xff, (temp >> 24) & 0xff,
+		(uintptr_t)handoff_address);
+
+	log_info("%s: Handoff data =\n{\n", __func__);
+
 	temp = readl(handoff_address + SOC64_HANDOFF_OFFSET_DATA +
 		    (i * sizeof(u32)));
 
-	if (endian_t == BIG_ENDIAN) {
-		debug("%s: Handoff addr = 0x%p ", __func__, (u32 *)handoff_address);
-		debug("Handoff table address = 0x%p ", table_x32);
-		debug("table length = 0x%x\n", table_len);
-		debug("%s: handoff data =\n{\n", __func__);
+	if (endian_t == BIG_ENDIAN)
 		*table_x32 = swab32(temp);
-	} else if (endian_t == LITTLE_ENDIAN) {
-		debug(" {\n");
+	else if (endian_t == LITTLE_ENDIAN)
 		*table_x32 = temp;
-	}
 
-	debug(" No.%d Addr 0x%08x: ", i, *table_x32);
+	log_info(" 0x%08x -> 0x%08x ", i, *table_x32);
 
 	for (i = 1; i < table_len; i++) {
 		table_x32++;
@@ -139,12 +141,12 @@ int socfpga_handoff_read(void *handoff_address, void *table, u32 table_len)
 			*table_x32 = temp;
 
 		if (!(i % 2))
-			debug(" No.%d Addr 0x%08x: ", i,
-			      *table_x32);
+			log_info(" 0x%08x -> 0x%08x ", i,
+				 *table_x32);
 		else
-			debug(" 0x%08x\n", *table_x32);
+			log_info("0x%08x\n", *table_x32);
 	}
-	debug("\n}\n");
+	log_info("}\n");
 
 	return 0;
 }

arch/arm/mach-socfpga/wrap_pll_config_soc64.c
@@ -4,6 +4,7 @@
  *
  */
 
+#include <log.h>
 #include <asm/arch/clock_manager.h>
 #include <asm/io.h>
 #include <asm/arch/handoff_soc64.h>
@@ -17,6 +18,17 @@ const struct cm_config * const cm_get_default_config(void)
 	u32 *conversion = (u32 *)cm_handoff_cfg;
 	u32 i;
 	u32 handoff_clk = readl(SOC64_HANDOFF_CLOCK);
+
+	log_info("%s : line %d => Handoff section : [%c%c%c%c] at 0x%08lx\n\n",
+		 __FILE__, __LINE__, (handoff_clk >> 0) & 0xff,
+		 (handoff_clk >> 8) & 0xff, (handoff_clk >> 16) & 0xff,
+		 (handoff_clk >> 24) & 0xff, (uintptr_t)SOC64_HANDOFF_CLOCK);
+
+	log_info("%s: Handoff data =\n{\n", __func__);
+	for (i = 0; i < (sizeof(*cm_handoff_cfg) / sizeof(u32)); i++)
+		log_info(" 0x%08x -> 0x%08x\n", i, readl(conversion + i));
+
+	log_info("}\n");
 
 	if (swab32(handoff_clk) == SOC64_HANDOFF_MAGIC_CLOCK) {
 		writel(swab32(handoff_clk), SOC64_HANDOFF_CLOCK);

common/Kconfig
@@ -107,7 +107,7 @@ config LOGLEVEL
 config SPL_LOGLEVEL
 	int
 	depends on SPL
-	default LOGLEVEL
+	default 6
 
 config TPL_LOGLEVEL
 	int
@@ -207,12 +207,35 @@ config PRE_CONSOLE_BUFFER
 	  initialised to a buffer. The buffer is a circular buffer, so
 	  if it overflows, earlier output is discarded.
 
-	  Note that this is not currently supported in SPL. It would be
-	  useful to be able to share the pre-console buffer with SPL.
+config SPL_PRE_CONSOLE_BUFFER
+	bool "Buffer characters before the console is available in SPL"
+	depends on SPL
+	help
+	  Prior to the console being initialised (i.e. serial UART
+	  initialised etc) all console output is silently discarded.
+	  Defining CONFIG_PRE_CONSOLE_BUFFER will cause U-Boot to
+	  buffer any console messages prior to the console being
+	  initialised to a buffer. The buffer is a circular buffer, so
+	  if it overflows, earlier output is discarded.
 
 config PRE_CON_BUF_SZ
 	int "Sets the size of the pre-console buffer"
 	depends on PRE_CONSOLE_BUFFER
+	default 4096
+	help
+	  The size of the pre-console buffer affects how much console output
+	  can be held before it overflows and starts discarding earlier
+	  output. Normally there is very little output at this early stage,
+	  unless debugging is enabled, so allow enough for ~10 lines of
+	  text.
+
+	  This is a useful feature if you are using a video console and
+	  want to see the full boot output on the console. Without this
+	  option only the post-relocation output will be displayed.
+
+config SPL_PRE_CON_BUF_SZ
+	int "Sets the size of the pre-console buffer in SPL"
+	depends on SPL_PRE_CONSOLE_BUFFER
 	default 4096
 	help
 	  The size of the pre-console buffer affects how much console output
@@ -240,6 +263,16 @@ config PRE_CON_BUF_ADDR
 
 	  We should consider removing this option and allocating the memory
 	  in board_init_f_init_reserve() instead.
+
+config SPL_PRE_CON_BUF_ADDR
+	hex "Address of the pre-console buffer in SPL"
+	depends on SPL_PRE_CONSOLE_BUFFER
+
+	help
+	  This sets the start address of the pre-console buffer. This must
+	  be in available memory and is accessed before relocation and
+	  possibly before DRAM is set up. Therefore choose an address
+	  carefully.
 
 config CONSOLE_FLUSH_SUPPORT
 	bool "Enable console flush support"

common/memsize.c
@@ -6,6 +6,7 @@
 
 #include <config.h>
 #include <init.h>
+#include <log.h>
 #include <asm/global_data.h>
 #include <cpu_func.h>
 #include <stdint.h>
@@ -57,11 +58,17 @@ long get_ram_size(long *base, long maxsize)
 	if (!CONFIG_IS_ENABLED(SYS_DCACHE_OFF))
 		dcache_en = dcache_status();
 
+	log_info("\n%s : Func %s - line %d => Start backup original content\n",
+		 __FILE__, __func__, __LINE__);
 	for (cnt = (maxsize / sizeof(long)) >> 1; cnt > 0; cnt >>= 1) {
 		addr = base + cnt;	/* pointer arith! */
 		sync();
+		log_info("Backup 0x%lx -> 0x%lx\n", (ulong)addr,
+			 (ulong)save[i]);
 		save[i++] = *addr;
 		sync();
+		log_info("Address : 0x%lx, Value written : 0x%lx\n",
+			 (ulong)addr, ~cnt);
 		*addr = ~cnt;
 		if (dcache_en)
 			dcache_flush_invalidate(addr);
@@ -77,32 +84,47 @@ long get_ram_size(long *base, long maxsize)
 	if (dcache_en)
 		dcache_flush_invalidate(addr);
 
+	log_info("\nFunc %s - line %d => Testing memory ...\n ", __func__,
+		 __LINE__);
 	if ((val = *addr) != 0) {
+		log_info("Error! Content of 0x%lx is not 0x0, expect 0x0\n",
+			 (ulong)addr);
 		/* Restore the original data before leaving the function. */
 		sync();
 		*base = save_base;
+		log_info("Original content restore start ...\n");
 		for (cnt = 1; cnt < maxsize / sizeof(long); cnt <<= 1) {
 			addr  = base + cnt;
 			sync();
+			log_info("Address : 0x%lx -> 0x%lx\n", (ulong)addr,
+				 (ulong)save[i - 1]);
 			*addr = save[--i];
 		}
 		return (0);
 	}
 
+	log_info("Memory test reading at ...\n");
 	for (cnt = 1; cnt < maxsize / sizeof(long); cnt <<= 1) {
 		addr = base + cnt;	/* pointer arith! */
 		val = *addr;
+		log_info("Address : 0x%lx -> 0x%lx\n", (ulong)addr,
+			 (ulong)val);
 		*addr = save[--i];
 		if (val != ~cnt) {
+			log_info("Error! Addr 0x%lx val 0x%lx != ~cnt 0x%lx\n",
+				 (ulong)addr, val, ~cnt);
 			size = cnt * sizeof(long);
 			/*
 			 * Restore the original data
 			 * before leaving the function.
 			 */
+			log_info("Original content restore start ...\n");
 			for (cnt <<= 1;
 			     cnt < maxsize / sizeof(long);
 			     cnt <<= 1) {
 				addr  = base + cnt;
+				log_info("Address : 0x%lx -> 0x%lx\n",
+					 (ulong)addr, (ulong)save[i - 1]);
 				*addr = save[--i];
 			}
 			/* warning: don't restore save_base in this case,
@@ -116,6 +138,8 @@ long get_ram_size(long *base, long maxsize)
 	}
 	*base = save_base;
 
+		log_info("\n%s : Func %s - line %d => Memory test passed!\n ",
+			 __FILE__, __func__, __LINE__);
 	return (maxsize);
 }
 

configs/socfpga_agilex5_defconfig
@@ -12,7 +12,7 @@ CONFIG_DM_GPIO=y
 CONFIG_DEFAULT_DEVICE_TREE="socfpga_agilex5_socdk"
 CONFIG_SPL_MMC=y
 CONFIG_DM_RESET=y
-CONFIG_SPL_STACK=0x71000
+CONFIG_SPL_STACK=0x6e000
 CONFIG_SPL_HAS_BSS_LINKER_SECTION=y
 CONFIG_SPL_BSS_START_ADDR=0x9ff00000
 CONFIG_SPL_BSS_MAX_SIZE=0x100000
@@ -151,3 +151,11 @@ CONFIG_DESIGNWARE_WATCHDOG=y
 CONFIG_WDT=y
 # CONFIG_SPL_USE_TINY_PRINTF is not set
 CONFIG_PANIC_HANG=y
+CONFIG_LOGLEVEL=7
+CONFIG_LOG_MAX_LEVEL=6
+CONFIG_SPL_LOG_MAX_LEVEL=5
+CONFIG_SPL_PRE_CONSOLE_BUFFER=y
+CONFIG_SPL_PRE_CON_BUF_ADDR=0x6e000
+CONFIG_SPL_PRE_CON_BUF_SZ=12288
+CONFIG_SPL_LOG=y
+CONFIG_LOG=y

arch/arm/mach-socfpga/spl_agilex5.c
@@ -5,6 +5,7 @@
  *
  */
 
+#include <console.h>
 #include <init.h>
 #include <asm/global_data.h>
 #include <asm/io.h>
@@ -86,8 +87,8 @@ void board_init_f(ulong dummy)
 		if (CONFIG_IS_ENABLED(WDT))
 			initr_watchdog();
 	}
-
 	preloader_console_init();
+	console_init_f();
 	print_reset_info();
 	cm_print_clock_quick_summary();
 

commit 8bc6fea1e348f320cd484c5e3bf5a956bdcfa2bc

HSD #15018318288: arm: socfpga: Improve debug toolkit
Replace U-Boot log framework with printk-style pr_info for
uniform suppression.

The original Agilex5 debug toolkit used U-Boot’s log framework
(LOG_MAX_LEVEL) to control UART output. On non‑Agilex5 platforms where
the log framework isn’t enabled, messages were still printed due to
default suppression behavior. Switch to printk-style pr_info() for a
single, cross-platform control point. This aligns output suppression with
CONFIG_LOGLEVEL, ensuring consistent visibility across all devices.

Fixes: c29e714f0395 ("HSD #15018318288: arm: socfpga: agilex5: Add debug toolkit")

Signed-off-by: Tien Fong Chee <tien.fong.chee@altera.com>

arch/arm/mach-socfpga/clock_manager.c
@@ -3,7 +3,6 @@
  *  Copyright (C) 2013-2017 Altera Corporation <www.altera.com>
  */
 
-#include <log.h>
 #include <asm/arch/clock_manager.h>
 #include <asm/arch/system_manager.h>
 #include <asm/global_data.h>
@@ -11,6 +10,7 @@
 #include <command.h>
 #include <init.h>
 #include <wait_bit.h>
+#include <linux/printk.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -19,8 +19,8 @@ void cm_wait_for_lock(u32 mask)
 	u32 inter_val;
 	u32 retry = 0;
 
-	log_info("%s : line %d => Is clock PLL locked? Checking ...\n",
-		 __FILE__, __LINE__);
+	pr_info("%s : line %d => Is clock PLL locked? Checking ...\n",
+		__FILE__, __LINE__);
 
 	do {
 #if defined(CONFIG_TARGET_SOCFPGA_GEN5)
@@ -39,8 +39,8 @@ void cm_wait_for_lock(u32 mask)
 			break;
 	} while (1);
 
-	log_info("%s : line %d => Clock PLL is locked !\n",
-		 __FILE__, __LINE__);
+	pr_info("%s : line %d => Clock PLL is locked !\n",
+		__FILE__, __LINE__);
 }
 
 /* function to poll in the fsm busy bit */

arch/arm/mach-socfpga/wrap_handoff_soc64.c
@@ -6,9 +6,9 @@
  */
 #include <errno.h>
 #include <inttypes.h>
-#include <log.h>
 #include <asm/arch/handoff_soc64.h>
 #include <asm/io.h>
+#include <linux/printk.h>
 
 #ifndef __ASSEMBLY__
 #include <asm/types.h>
@@ -62,9 +62,9 @@ static int getting_endianness(void *handoff_address, enum endianness *endian_t)
 		/* Trying to check handoff data is big endian? */
 		*endian_t = check_endianness(swab32(readl(handoff_address)));
 		if (*endian_t == UNKNOWN_ENDIANNESS) {
-			log_info("%s : line %d => Cannot find endianness ", __FILE__,
-				 __LINE__);
-			log_info("at addr 0x%p\n", (u32 *)handoff_address);
+			pr_info("%s : line %d => Cannot find endianness ", __FILE__,
+				__LINE__);
+			pr_info("at addr 0x%p\n", (u32 *)handoff_address);
 			return -EPERM;
 		}
 	}
@@ -88,8 +88,8 @@ int socfpga_get_handoff_size(void *handoff_address)
 
 	size = (size - SOC64_HANDOFF_OFFSET_DATA) / sizeof(u32);
 
-	log_info("%s : line %d => handoff address = 0x%p handoff size = 0x%08x\n", __FILE__,
-		 __LINE__, (u32 *)handoff_address, size);
+	pr_info("%s : line %d => handoff address = 0x%p handoff size = 0x%08x\n", __FILE__,
+		__LINE__, (u32 *)handoff_address, size);
 
 	return size;
 }
@@ -106,17 +106,17 @@ int socfpga_handoff_read(void *handoff_address, void *table, u32 table_len)
 	if (ret)
 		return ret;
 
-	log_info("%s : line %d => Handoff table address = 0x%p, ", __FILE__,
-		 __LINE__, table_x32);
-	log_info("Table length = 0x%x\n\n", table_len);
+	pr_info("%s : line %d => Handoff table address = 0x%p, ", __FILE__,
+		__LINE__, table_x32);
+	pr_info("Table length = 0x%x\n\n", table_len);
 
 	temp = readl(handoff_address);
-	log_info("%s : line %d => Handoff section : [%c%c%c%c] at 0x%08lx\n\n",
-		 __FILE__, __LINE__, (temp >> 0) & 0xff, (temp >> 8) & 0xff,
+	pr_info("%s : line %d => Handoff section : [%c%c%c%c] at 0x%08lx\n\n",
+		__FILE__, __LINE__, (temp >> 0) & 0xff, (temp >> 8) & 0xff,
 		(temp >> 16) & 0xff, (temp >> 24) & 0xff,
 		(uintptr_t)handoff_address);
 
-	log_info("%s: Handoff data =\n{\n", __func__);
+	pr_info("%s: Handoff data =\n{\n", __func__);
 
 	temp = readl(handoff_address + SOC64_HANDOFF_OFFSET_DATA +
 		    (i * sizeof(u32)));
@@ -126,7 +126,7 @@ int socfpga_handoff_read(void *handoff_address, void *table, u32 table_len)
 	else if (endian_t == LITTLE_ENDIAN)
 		*table_x32 = temp;
 
-	log_info(" 0x%08x -> 0x%08x ", i, *table_x32);
+	pr_info(" 0x%08x -> 0x%08x ", i, *table_x32);
 
 	for (i = 1; i < table_len; i++) {
 		table_x32++;
@@ -141,12 +141,12 @@ int socfpga_handoff_read(void *handoff_address, void *table, u32 table_len)
 			*table_x32 = temp;
 
 		if (!(i % 2))
-			log_info(" 0x%08x -> 0x%08x ", i,
-				 *table_x32);
+			pr_info(" 0x%08x -> 0x%08x ", i,
+				*table_x32);
 		else
-			log_info("0x%08x\n", *table_x32);
+			pr_info("0x%08x\n", *table_x32);
 	}
-	log_info("}\n");
+	pr_info("}\n");
 
 	return 0;
 }

arch/arm/mach-socfpga/wrap_pll_config_soc64.c
@@ -4,11 +4,11 @@
  *
  */
 
-#include <log.h>
 #include <asm/arch/clock_manager.h>
 #include <asm/io.h>
 #include <asm/arch/handoff_soc64.h>
 #include <asm/arch/system_manager.h>
+#include <linux/printk.h>
 
 const struct cm_config * const cm_get_default_config(void)
 {
@@ -19,16 +19,16 @@ const struct cm_config * const cm_get_default_config(void)
 	u32 i;
 	u32 handoff_clk = readl(SOC64_HANDOFF_CLOCK);
 
-	log_info("%s : line %d => Handoff section : [%c%c%c%c] at 0x%08lx\n\n",
-		 __FILE__, __LINE__, (handoff_clk >> 0) & 0xff,
-		 (handoff_clk >> 8) & 0xff, (handoff_clk >> 16) & 0xff,
-		 (handoff_clk >> 24) & 0xff, (uintptr_t)SOC64_HANDOFF_CLOCK);
+	pr_info("%s : line %d => Handoff section : [%c%c%c%c] at 0x%08lx\n\n",
+		__FILE__, __LINE__, (handoff_clk >> 0) & 0xff,
+		(handoff_clk >> 8) & 0xff, (handoff_clk >> 16) & 0xff,
+		(handoff_clk >> 24) & 0xff, (uintptr_t)SOC64_HANDOFF_CLOCK);
 
-	log_info("%s: Handoff data =\n{\n", __func__);
+	pr_info("%s: Handoff data =\n{\n", __func__);
 	for (i = 0; i < (sizeof(*cm_handoff_cfg) / sizeof(u32)); i++)
-		log_info(" 0x%08x -> 0x%08x\n", i, readl(conversion + i));
+		pr_info(" 0x%08x -> 0x%08x\n", i, readl(conversion + i));
 
-	log_info("}\n");
+	pr_info("}\n");
 
 	if (swab32(handoff_clk) == SOC64_HANDOFF_MAGIC_CLOCK) {
 		writel(swab32(handoff_clk), SOC64_HANDOFF_CLOCK);

common/Kconfig
@@ -107,7 +107,7 @@ config LOGLEVEL
 config SPL_LOGLEVEL
 	int
 	depends on SPL
-	default 6
+	default LOGLEVEL
 
 config TPL_LOGLEVEL
 	int

common/memsize.c
@@ -6,10 +6,10 @@
 
 #include <config.h>
 #include <init.h>
-#include <log.h>
 #include <asm/global_data.h>
 #include <cpu_func.h>
 #include <stdint.h>
+#include <linux/printk.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -58,17 +58,17 @@ long get_ram_size(long *base, long maxsize)
 	if (!CONFIG_IS_ENABLED(SYS_DCACHE_OFF))
 		dcache_en = dcache_status();
 
-	log_info("\n%s : Func %s - line %d => Start backup original content\n",
-		 __FILE__, __func__, __LINE__);
+	pr_info("\n%s : Func %s - line %d => Start backup original content\n",
+		__FILE__, __func__, __LINE__);
 	for (cnt = (maxsize / sizeof(long)) >> 1; cnt > 0; cnt >>= 1) {
 		addr = base + cnt;	/* pointer arith! */
 		sync();
-		log_info("Backup 0x%lx -> 0x%lx\n", (ulong)addr,
-			 (ulong)save[i]);
+		pr_info("Backup 0x%lx -> 0x%lx\n", (ulong)addr,
+			(ulong)save[i]);
 		save[i++] = *addr;
 		sync();
-		log_info("Address : 0x%lx, Value written : 0x%lx\n",
-			 (ulong)addr, ~cnt);
+		pr_info("Address : 0x%lx, Value written : 0x%lx\n",
+			(ulong)addr, ~cnt);
 		*addr = ~cnt;
 		if (dcache_en)
 			dcache_flush_invalidate(addr);
@@ -84,47 +84,47 @@ long get_ram_size(long *base, long maxsize)
 	if (dcache_en)
 		dcache_flush_invalidate(addr);
 
-	log_info("\nFunc %s - line %d => Testing memory ...\n ", __func__,
-		 __LINE__);
+	pr_info("\nFunc %s - line %d => Testing memory ...\n ", __func__,
+		__LINE__);
 	if ((val = *addr) != 0) {
-		log_info("Error! Content of 0x%lx is not 0x0, expect 0x0\n",
-			 (ulong)addr);
+		pr_info("Error! Content of 0x%lx is not 0x0, expect 0x0\n",
+			(ulong)addr);
 		/* Restore the original data before leaving the function. */
 		sync();
 		*base = save_base;
-		log_info("Original content restore start ...\n");
+		pr_info("Original content restore start ...\n");
 		for (cnt = 1; cnt < maxsize / sizeof(long); cnt <<= 1) {
 			addr  = base + cnt;
 			sync();
-			log_info("Address : 0x%lx -> 0x%lx\n", (ulong)addr,
-				 (ulong)save[i - 1]);
+			pr_info("Address : 0x%lx -> 0x%lx\n", (ulong)addr,
+				(ulong)save[i - 1]);
 			*addr = save[--i];
 		}
 		return (0);
 	}
 
-	log_info("Memory test reading at ...\n");
+	pr_info("Memory test reading at ...\n");
 	for (cnt = 1; cnt < maxsize / sizeof(long); cnt <<= 1) {
 		addr = base + cnt;	/* pointer arith! */
 		val = *addr;
-		log_info("Address : 0x%lx -> 0x%lx\n", (ulong)addr,
-			 (ulong)val);
+		pr_info("Address : 0x%lx -> 0x%lx\n", (ulong)addr,
+			(ulong)val);
 		*addr = save[--i];
 		if (val != ~cnt) {
-			log_info("Error! Addr 0x%lx val 0x%lx != ~cnt 0x%lx\n",
-				 (ulong)addr, val, ~cnt);
+			pr_info("Error! Addr 0x%lx val 0x%lx != ~cnt 0x%lx\n",
+				(ulong)addr, val, ~cnt);
 			size = cnt * sizeof(long);
 			/*
 			 * Restore the original data
 			 * before leaving the function.
 			 */
-			log_info("Original content restore start ...\n");
+			pr_info("Original content restore start ...\n");
 			for (cnt <<= 1;
 			     cnt < maxsize / sizeof(long);
 			     cnt <<= 1) {
 				addr  = base + cnt;
-				log_info("Address : 0x%lx -> 0x%lx\n",
-					 (ulong)addr, (ulong)save[i - 1]);
+				pr_info("Address : 0x%lx -> 0x%lx\n",
+					(ulong)addr, (ulong)save[i - 1]);
 				*addr = save[--i];
 			}
 			/* warning: don't restore save_base in this case,
@@ -138,8 +138,8 @@ long get_ram_size(long *base, long maxsize)
 	}
 	*base = save_base;
 
-		log_info("\n%s : Func %s - line %d => Memory test passed!\n ",
-			 __FILE__, __func__, __LINE__);
+		pr_info("\n%s : Func %s - line %d => Memory test passed!\n ",
+			__FILE__, __func__, __LINE__);
 	return (maxsize);
 }
 

configs/socfpga_agilex5_defconfig
@@ -151,11 +151,7 @@ CONFIG_DESIGNWARE_WATCHDOG=y
 CONFIG_WDT=y
 # CONFIG_SPL_USE_TINY_PRINTF is not set
 CONFIG_PANIC_HANG=y
-CONFIG_LOGLEVEL=7
-CONFIG_LOG_MAX_LEVEL=6
-CONFIG_SPL_LOG_MAX_LEVEL=5
+CONFIG_LOGLEVEL=6
 CONFIG_SPL_PRE_CONSOLE_BUFFER=y
 CONFIG_SPL_PRE_CON_BUF_ADDR=0x6e000
 CONFIG_SPL_PRE_CON_BUF_SZ=12288
-CONFIG_SPL_LOG=y
-CONFIG_LOG=y